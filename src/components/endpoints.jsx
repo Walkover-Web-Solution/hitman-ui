f27143e80d88906b0c8597ddaf1940749ac84849
Umasankar Cherukuri <umasankar@walkover.in>
2020-02-25 15:38:43 +0530

    fix: move endpoint using dnd

--- src/components/endpoints.jsx
+++ src/components/endpoints.jsx
@@ -1,166 +1,170 @@
-import React, { Component } from 'react';
-import { Accordion, Card, Button, Dropdown, DropdownButton, Table } from 'react-bootstrap';
+import React, { Component } from "react";
+import {
+  Accordion,
+  Card,
+  Button,
+  Dropdown,
+  DropdownButton,
+  Table
+} from "react-bootstrap";
 
 class Endpoints extends Component {
-	state = {};
+  state = {};
 
-	onDragStart = (e, eId) => {
-		this.props.group_dnd(false);
-		this.draggedItem = eId;
-		this.props.set_dnd(eId, this.props.group_id);
-	};
+  onDragStart = (e, eId) => {
+    this.props.group_dnd(false);
+    this.draggedItem = eId;
+    this.props.set_dnd(eId, this.props.group_id);
+  };
 
-	onDragOver = (e, eId) => {
-		e.preventDefault();
-		this.draggedOverItem = eId;
-	};
+  onDragOver = (e, eId) => {
+    e.preventDefault();
+    this.draggedOverItem = eId;
+  };
 
-	async onDragEnd(e, props) {
-		this.props.group_dnd(true);
-		if (this.draggedItem === this.draggedOverItem) {
-			this.draggedItem = null;
-			return;
-		}
-		let endpointIds = this.props.endpoints_order.filter((item) => item !== this.draggedItem);
-		const index = this.props.endpoints_order.findIndex((eId) => eId === this.draggedOverItem);
-		endpointIds.splice(index, 0, this.draggedItem);
-		this.props.set_endpoint_id(this.props.group_id, endpointIds);
-		this.draggedItem = null;
-	}
+  async onDragEnd(e, props) {
+    this.props.group_dnd(true);
+    if (this.draggedItem === this.draggedOverItem) {
+      this.draggedItem = null;
+      return;
+    }
+    let endpointIds = this.props.endpoints_order.filter(
+      item => item !== this.draggedItem
+    );
+    const index = this.props.endpoints_order.findIndex(
+      eId => eId === this.draggedOverItem
+    );
+    endpointIds.splice(index, 0, this.draggedItem);
+    this.props.set_endpoint_id(this.props.group_id, endpointIds);
+    this.draggedItem = null;
+  }
 
-	onDrop = (e, eId) => {
-		e.preventDefault();
-		if (!this.draggedItem) {
-			this.props.get_dnd(eId, this.props.group_id);
-		}
-		// this.draggedItem = null
-	};
+  onDrop = e => {
+    e.preventDefault();
+    if (!this.draggedItem) {
+      this.props.get_dnd(this.props.group_id);
+    }
+    // this.draggedItem = null
+  };
 
-	handleDelete(endpoint) {
-		this.props.history.push({
-			pathname: '/dashboard/collections',
-			deleteEndpointId: endpoint.id,
-			groupId: this.props.group_id
-		});
-	}
+  handleDelete(endpoint) {
+    this.props.history.push({
+      pathname: "/dashboard/collections",
+      deleteEndpointId: endpoint.id,
+      groupId: this.props.group_id
+    });
+  }
 
-	handleDuplicate(endpoint) {
-		this.props.history.push({
-			pathname: '/dashboard/collections',
-			duplicateEndpoint: endpoint
-		});
-	}
+  handleDuplicate(endpoint) {
+    this.props.history.push({
+      pathname: "/dashboard/collections",
+      duplicateEndpoint: endpoint
+    });
+  }
 
-	handleUpdate(endpoint) {
-		this.props.history.push({
-			pathname: `/dashboard/collections/${this.props.collection_id}/versions/${this.props
-				.version_id}/groups/${this.props.group_id}/endpoints/${endpoint.id}/edit`,
-			editEndpoint: endpoint
-		});
-	}
-	handleDisplay(endpoint, groups, versions, groupId) {
-		this.props.history.push({
-			pathname: `/dashboard/collections/endpoints/${endpoint.id}`,
-			title: 'update endpoint',
-			endpoint: endpoint,
-			groupId: groupId,
-			groups: groups,
-			versions: versions,
-			endpointFlag: true
-		});
-	}
-	render() {
-		if (this.props.endpoints_order.length == 0) {
-			return (
-				// <Accordion
-				//   draggable
-				// onDragOver={e => this.onDragOver(e)}
-				// onDragStart={e => this.onDragStart(e)}
-				// onDragEnd={e => this.onDragEnd(e, this.props)}
-				// onDrop={e => { console.log("this.onDrop(e, this.props)") }}>
-				// <Card>
-				//     <Accordion.Toggle as={text}>
-				//       This group is empty
-				//   </Accordion.Toggle>
-				//   </Card>
-				// </Accordion>
-				<Table
-					striped
-					bordered
-					hover
-					size="sm"
-					onDragOver={(e) => this.onDragOver(e)}
-					onDragStart={(e) => this.onDragStart(e)}
-					onDragEnd={(e) => this.onDragEnd(e, this.props)}
-					onDrop={(e) => {
-						console.log('dropped on empty group');
-					}}
-				>
-					<thead>
-						<tr>
-							<th>This group is empty</th>
-						</tr>
-					</thead>
-				</Table>
-			);
-		} else {
-			return (
-				<div>
-					{this.props.endpoints &&
-						this.props.endpoints_order
-							.filter((eId) => this.props.endpoints[eId].groupId === this.props.group_id)
-							.map((endpointId) => (
-								<Accordion defaultActiveKey="1" key={endpointId}>
-									<Card>
-										<Card.Header
-											draggable
-											onDragOver={(e) => this.onDragOver(e, endpointId)}
-											onDragStart={(e) => this.onDragStart(e, endpointId)}
-											onDragEnd={(e) => this.onDragEnd(e, endpointId, this.props)}
-											onDrop={(e) => this.onDrop(e, endpointId, this.props)}
-										>
-											<Accordion.Toggle
-												onClick={() =>
-													this.handleDisplay(
-														this.props.endpoints[endpointId],
-														this.props.groups,
-														this.props.versions,
-														this.props.group_id
-													)}
-												as={Button}
-												variant="link"
-												eventKey="1"
-											>
-												{this.props.endpoints[endpointId].name}
-											</Accordion.Toggle>
-											<DropdownButton
-												alignRight
-												title=""
-												id="dropdown-menu-align-right"
-												style={{ float: 'right' }}
-											>
-												<Dropdown.Item
-													eventKey="2"
-													onClick={() => this.handleDelete(this.props.endpoints[endpointId])}
-												>
-													Delete
-												</Dropdown.Item>
-												<Dropdown.Item
-													eventKey="3"
-													onClick={() =>
-														this.handleDuplicate(this.props.endpoints[endpointId])}
-												>
-													Duplicate
-												</Dropdown.Item>
-											</DropdownButton>
-										</Card.Header>
-									</Card>
-								</Accordion>
-							))}
-				</div>
-			);
-		}
-	}
+  handleUpdate(endpoint) {
+    this.props.history.push({
+      pathname: `/dashboard/collections/${this.props.collection_id}/versions/${this.props.version_id}/groups/${this.props.group_id}/endpoints/${endpoint.id}/edit`,
+      editEndpoint: endpoint
+    });
+  }
+  handleDisplay(endpoint, groups, versions, groupId) {
+    this.props.history.push({
+      pathname: `/dashboard/collections/endpoints/${endpoint.id}`,
+      title: "update endpoint",
+      endpoint: endpoint,
+      groupId: groupId,
+      groups: groups,
+      versions: versions,
+      endpointFlag: true
+    });
+  }
+  render() {
+    if (this.props.endpoints_order.length == 0) {
+      return (
+        <Table
+          striped
+          bordered
+          hover
+          size="sm"
+          onDragOver={e => this.onDragOver(e)}
+          onDragStart={e => this.onDragStart(e)}
+          onDragEnd={e => this.onDragEnd(e, this.props)}
+          onDrop={e => this.onDrop(e)}
+        >
+          <thead>
+            <tr>
+              <th>This group is empty</th>
+            </tr>
+          </thead>
+        </Table>
+      );
+    } else {
+      return (
+        <div>
+          {this.props.endpoints &&
+            this.props.endpoints_order
+              .filter(
+                eId => this.props.endpoints[eId].groupId === this.props.group_id
+              )
+              .map(endpointId => (
+                <Accordion defaultActiveKey="1" key={endpointId}>
+                  <Card>
+                    <Card.Header
+                      draggable
+                      onDragOver={e => this.onDragOver(e, endpointId)}
+                      onDragStart={e => this.onDragStart(e, endpointId)}
+                      onDragEnd={e => this.onDragEnd(e, endpointId, this.props)}
+                      onDrop={e => this.onDrop(e)}
+                    >
+                      <Accordion.Toggle
+                        onClick={() =>
+                          this.handleDisplay(
+                            this.props.endpoints[endpointId],
+                            this.props.groups,
+                            this.props.versions,
+                            this.props.group_id
+                          )
+                        }
+                        as={Button}
+                        variant="link"
+                        eventKey="1"
+                      >
+                        {this.props.endpoints[endpointId].name}
+                      </Accordion.Toggle>
+                      <DropdownButton
+                        alignRight
+                        title=""
+                        id="dropdown-menu-align-right"
+                        style={{ float: "right" }}
+                      >
+                        <Dropdown.Item
+                          eventKey="2"
+                          onClick={() =>
+                            this.handleDelete(this.props.endpoints[endpointId])
+                          }
+                        >
+                          Delete
+                        </Dropdown.Item>
+                        <Dropdown.Item
+                          eventKey="3"
+                          onClick={() =>
+                            this.handleDuplicate(
+                              this.props.endpoints[endpointId]
+                            )
+                          }
+                        >
+                          Duplicate
+                        </Dropdown.Item>
+                      </DropdownButton>
+                    </Card.Header>
+                  </Card>
+                </Accordion>
+              ))}
+        </div>
+      );
+    }
+  }
 }
 
 export default Endpoints;
--- src/components/groups.jsx
+++ src/components/groups.jsx
@@ -1,198 +1,221 @@
-import React, { Component } from 'react';
-import { Accordion, Card, Button, Dropdown, DropdownButton } from 'react-bootstrap';
-import GroupPages from './groupPages';
-import Endpoints from './endpoints';
+import React, { Component } from "react";
+import {
+  Accordion,
+  Card,
+  Button,
+  Dropdown,
+  DropdownButton
+} from "react-bootstrap";
+import GroupPages from "./groupPages";
+import Endpoints from "./endpoints";
 
 class Groups extends Component {
-	state = {
-		groupDnDFlag: true
-	};
+  state = {
+    groupDnDFlag: true
+  };
 
-	setDnD(draggedEndpoint, groupId) {
-		this.draggedEndpoint = draggedEndpoint;
-		this.sourceGroupId = groupId;
-	}
+  setDnD(draggedEndpoint, groupId) {
+    this.draggedEndpoint = draggedEndpoint;
+    this.sourceGroupId = groupId;
+  }
 
-	getDnD(draggedOverEndpoint, destinationGroupId) {
-		this.draggedOverEndpoint = draggedOverEndpoint;
-		this.props.dnd_move_endpoint(draggedOverEndpoint, this.draggedEndpoint, this.sourceGroupId, destinationGroupId);
-	}
+  getDnD(destinationGroupId) {
+    this.props.dnd_move_endpoint(
+      this.draggedEndpoint,
+      this.sourceGroupId,
+      destinationGroupId
+    );
+  }
 
-	groupDnD(groupDnDFlag) {
-		this.props.version_dnd(groupDnDFlag);
-		this.setState({ groupDnDFlag });
-	}
+  groupDnD(groupDnDFlag) {
+    this.props.version_dnd(groupDnDFlag);
+    this.setState({ groupDnDFlag });
+  }
 
-	onDragStart = (e, groupId) => {
-		this.props.version_dnd(false);
-		this.draggedItem = groupId;
-	};
+  onDragStart = (e, groupId) => {
+    this.props.version_dnd(false);
+    this.draggedItem = groupId;
+  };
 
-	onDragOver = (e, groupId) => {
-		e.preventDefault();
-		this.draggedOverItem = groupId;
-	};
+  onDragOver = (e, groupId) => {
+    e.preventDefault();
+    this.draggedOverItem = groupId;
+  };
 
-	async onDragEnd(e, props) {
-		this.props.version_dnd(true);
-		if (this.draggedItem === this.draggedOverItem) {
-			return;
-		}
-		let groupIds = this.props.group_ids.filter((item) => item !== this.draggedItem);
-		const index = this.props.group_ids.findIndex((vId) => vId === this.draggedOverItem);
-		groupIds.splice(index, 0, this.draggedItem);
-		this.props.set_group_id(groupIds);
-	}
+  async onDragEnd(e, props) {
+    this.props.version_dnd(true);
+    if (this.draggedItem === this.draggedOverItem) {
+      return;
+    }
+    let groupIds = this.props.group_ids.filter(
+      item => item !== this.draggedItem
+    );
+    const index = this.props.group_ids.findIndex(
+      vId => vId === this.draggedOverItem
+    );
+    groupIds.splice(index, 0, this.draggedItem);
+    this.props.set_group_id(groupIds);
+  }
 
-	onDrop(destinationGroupId, props) {
-		if (!this.draggedItem) {
-			// console.log(`move ${this.draggedEndpoint}, from ${this.sourceGroupId} to ${destinationGroupId}`)
-			this.props.dnd_move_endpoint(this.draggedEndpoint, this.sourceGroupId, destinationGroupId);
-		}
-	}
+  onDrop(destinationGroupId, props) {
+    if (!this.draggedItem) {
+      // console.log(`move ${this.draggedEndpoint}, from ${this.sourceGroupId} to ${destinationGroupId}`)
+      this.props.dnd_move_endpoint(
+        this.draggedEndpoint,
+        this.sourceGroupId,
+        destinationGroupId
+      );
+    }
+  }
 
-	handleAddPage(groupId, versionId, collectionId) {
-		this.props.history.push({
-			pathname: `/dashboard/collections/${collectionId}/versions/${versionId}/groups/${groupId}/pages/new`,
-			versionId: versionId,
-			groupId: groupId
-		});
-	}
+  handleAddPage(groupId, versionId, collectionId) {
+    this.props.history.push({
+      pathname: `/dashboard/collections/${collectionId}/versions/${versionId}/groups/${groupId}/pages/new`,
+      versionId: versionId,
+      groupId: groupId
+    });
+  }
 
-	handleAddEndpoint(groupId, versionId, collectionId, versions, groups) {
-		this.props.history.push({
-			pathname: `/dashboard/collections/endpoints`,
-			versions: versions,
-			groups: groups,
-			groupId: groupId,
-			title: 'Add New Endpoint',
-			groupFlag: true
-		});
-	}
+  handleAddEndpoint(groupId, versionId, collectionId, versions, groups) {
+    this.props.history.push({
+      pathname: `/dashboard/collections/endpoints`,
+      versions: versions,
+      groups: groups,
+      groupId: groupId,
+      title: "Add New Endpoint",
+      groupFlag: true
+    });
+  }
 
-	handleDuplicate(group) {
-		this.props.history.push({
-			pathname: '/dashboard/collections',
-			duplicateGroup: group
-		});
-	}
+  handleDuplicate(group) {
+    this.props.history.push({
+      pathname: "/dashboard/collections",
+      duplicateGroup: group
+    });
+  }
 
-	render() {
-		return (
-			<div>
-				{this.props.groups &&
-					this.props.group_ids &&
-					this.props.group_ids
-						.filter((gId) => this.props.groups[gId].versionId === this.props.version_id)
-						.map((groupId) => (
-							<Accordion defaultActiveKey="0" key={groupId}>
-								<Card>
-									<Card.Header
-										draggable
-										onDragOver={(e) => this.onDragOver(e, groupId)}
-										onDragStart={(e) => this.onDragStart(e, groupId)}
-										onDragEnd={(e) => this.onDragEnd(e, groupId, this.props)}
-										onDrop={(e) => this.onDrop(groupId, this.props)}
-									>
-										<Accordion.Toggle as={Button} variant="link" eventKey="1">
-											{this.props.groups[groupId].name}
-										</Accordion.Toggle>
-										<DropdownButton
-											alignRight
-											title=""
-											id="dropdown-menu-align-right"
-											style={{ float: 'right' }}
-										>
-											<Dropdown.Item
-												eventKey="1"
-												onClick={() => {
-													this.props.history.push({
-														pathname: `/dashboard/collections/${this.props
-															.collection_id}/versions/${this.props
-															.version_id}/groups/${groupId}/edit`,
-														editGroup: this.props.groups[groupId]
-													});
-												}}
-											>
-												Edit
-											</Dropdown.Item>
-											<Dropdown.Item
-												eventKey="2"
-												onClick={() => {
-													if (
-														window.confirm(
-															'Are you sure you wish to delete this group? ' +
-																'\n' +
-																'All your pages and endpoints present in this group will be deleted.'
-														)
-													)
-														this.props.history.push({
-															pathname: '/dashboard/collections',
-															deletedGroupId: groupId
-														});
-												}}
-											>
-												Delete
-											</Dropdown.Item>
-											<Dropdown.Item
-												eventKey="3"
-												onClick={() =>
-													this.handleAddPage(
-														groupId,
-														this.props.groups[groupId].versionId,
-														this.props.collection_id
-													)}
-											>
-												Add Page
-											</Dropdown.Item>
-											<Dropdown.Item
-												eventKey="3"
-												onClick={() =>
-													this.handleAddEndpoint(
-														groupId,
-														this.props.groups[groupId].versionId,
-														this.props.collection_id,
-														this.props.versions,
-														this.props.groups
-													)}
-											>
-												Add Endpoint
-											</Dropdown.Item>
-											<Dropdown.Item
-												onClick={() => this.handleDuplicate(this.props.groups[groupId])}
-											>
-												Duplicate
-											</Dropdown.Item>
-										</DropdownButton>
-									</Card.Header>
-									<Accordion.Collapse eventKey="1">
-										<Card.Body>
-											<GroupPages
-												{...this.props}
-												version_id={this.props.groups[groupId].versionId}
-												group_id={groupId}
-												group_dnd={this.groupDnD.bind(this)}
-											/>
-											{/* </Card.Body>
+  render() {
+    return (
+      <div>
+        {this.props.groups &&
+          this.props.group_ids &&
+          this.props.group_ids
+            .filter(
+              gId => this.props.groups[gId].versionId === this.props.version_id
+            )
+            .map(groupId => (
+              <Accordion defaultActiveKey="0" key={groupId}>
+                <Card>
+                  <Card.Header
+                    draggable
+                    onDragOver={e => this.onDragOver(e, groupId)}
+                    onDragStart={e => this.onDragStart(e, groupId)}
+                    onDragEnd={e => this.onDragEnd(e, groupId, this.props)}
+                    onDrop={e => this.onDrop(groupId, this.props)}
+                  >
+                    <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                      {this.props.groups[groupId].name}
+                    </Accordion.Toggle>
+                    <DropdownButton
+                      alignRight
+                      title=""
+                      id="dropdown-menu-align-right"
+                      style={{ float: "right" }}
+                    >
+                      <Dropdown.Item
+                        eventKey="1"
+                        onClick={() => {
+                          this.props.history.push({
+                            pathname: `/dashboard/collections/${this.props.collection_id}/versions/${this.props.version_id}/groups/${groupId}/edit`,
+                            editGroup: this.props.groups[groupId]
+                          });
+                        }}
+                      >
+                        Edit
+                      </Dropdown.Item>
+                      <Dropdown.Item
+                        eventKey="2"
+                        onClick={() => {
+                          if (
+                            window.confirm(
+                              "Are you sure you wish to delete this group? " +
+                                "\n" +
+                                "All your pages and endpoints present in this group will be deleted."
+                            )
+                          )
+                            this.props.history.push({
+                              pathname: "/dashboard/collections",
+                              deletedGroupId: groupId
+                            });
+                        }}
+                      >
+                        Delete
+                      </Dropdown.Item>
+                      <Dropdown.Item
+                        eventKey="3"
+                        onClick={() =>
+                          this.handleAddPage(
+                            groupId,
+                            this.props.groups[groupId].versionId,
+                            this.props.collection_id
+                          )
+                        }
+                      >
+                        Add Page
+                      </Dropdown.Item>
+                      <Dropdown.Item
+                        eventKey="3"
+                        onClick={() =>
+                          this.handleAddEndpoint(
+                            groupId,
+                            this.props.groups[groupId].versionId,
+                            this.props.collection_id,
+                            this.props.versions,
+                            this.props.groups
+                          )
+                        }
+                      >
+                        Add Endpoint
+                      </Dropdown.Item>
+                      <Dropdown.Item
+                        onClick={() =>
+                          this.handleDuplicate(this.props.groups[groupId])
+                        }
+                      >
+                        Duplicate
+                      </Dropdown.Item>
+                    </DropdownButton>
+                  </Card.Header>
+                  <Accordion.Collapse eventKey="1">
+                    <Card.Body>
+                      <GroupPages
+                        {...this.props}
+                        version_id={this.props.groups[groupId].versionId}
+                        group_id={groupId}
+                        group_dnd={this.groupDnD.bind(this)}
+                      />
+                      {/* </Card.Body>
                   </Accordion.Collapse>
                   <Accordion.Collapse eventKey="1">
                     <Card.Body> */}
-											<Endpoints
-												{...this.props}
-												group_id={groupId}
-												endpoints_order={this.props.groups[groupId].endpointsOrder}
-												group_dnd={this.groupDnD.bind(this)}
-												set_dnd={this.setDnD.bind(this)}
-												get_dnd={this.getDnD.bind(this)}
-											/>
-										</Card.Body>
-									</Accordion.Collapse>
-								</Card>
-							</Accordion>
-						))}
-			</div>
-		);
-	}
+                      <Endpoints
+                        {...this.props}
+                        group_id={groupId}
+                        endpoints_order={
+                          this.props.groups[groupId].endpointsOrder
+                        }
+                        group_dnd={this.groupDnD.bind(this)}
+                        set_dnd={this.setDnD.bind(this)}
+                        get_dnd={this.getDnD.bind(this)}
+                      />
+                    </Card.Body>
+                  </Accordion.Collapse>
+                </Card>
+              </Accordion>
+            ))}
+      </div>
+    );
+  }
 }
 
 export default Groups;
