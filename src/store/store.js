6834f6268485d3d0adfe3d2679b00d7ae3e9e0a1
sourabh04 <sourabh@walkover.in>
2020-03-04 15:43:11 +0530

    fix

--- src/store/store.js
+++ src/store/store.js
@@ -1,26 +1,19 @@
 import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import collectionsReducer from "../components/collections/collectionsReducer";
-<<<<<<< HEAD
 import endpointsReducer from "../components/endpoints/endpointsReducer";
 import groupsReducer from "../components/groups/groupsReducer";
-=======
->>>>>>> versions-redux
 import thunk from "redux-thunk";
 import { logger } from "redux-logger";
 import versionsReducer from '../components/collectionVersions/collectionVersionsReducer';
 
 const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-<<<<<<< HEAD
 const rootReducer = combineReducers({
   collections: collectionsReducer,
+  versions: versionsReducer,
   groups: groupsReducer,
   endpoints: endpointsReducer
 });
-=======
-const rootReducer = combineReducers({ collections: collectionsReducer, versions: versionsReducer });
-
->>>>>>> versions-redux
 const store = createStore(
     rootReducer,
     storeEnhancers(applyMiddleware(thunk, logger))
