2e63f116d569d7e8cb20443c2a23de2894bc8f69
sourabh04 <sourabh@walkover.in>
2020-04-14 13:13:22 +0530

    raw body in public endpoint

--- src/components/endpoints/publicBodyContainer.jsx
+++ src/components/endpoints/publicBodyContainer.jsx
@@ -20,6 +20,30 @@ class PublicBodyContainer extends Component {
 
   render() {
     console.log(this.props);
+    if (this.props.body && this.props.body.type === "JSON") {
+      this.dataArray = [];
+      const jsonData = JSON.parse(this.props.body.value);
+      console.log(jsonData);
+      const keysArray = Object.keys(jsonData);
+      console.log(typeof jsonData[keysArray[1]]);
+      let i;
+      for (i in keysArray) {
+        // let thing = new JSONObject(jsonData).get(keysArray[i]);
+        // let classNameOfThing = thing.getClass().getName();
+        // console.log("thing is a ", classNameOfThing);
+        // if (thing instanceof Integer) {
+        //   System.out.println("thing is an Integer");
+        // }
+        const json = {
+          key: keysArray[i],
+          value: jsonData[keysArray[i]],
+          dataType: typeof jsonData[keysArray[i]],
+        };
+        this.dataArray.push(json);
+      }
+
+      console.log(this.dataArray);
+    }
     return (
       <div>
         {this.props.body && this.props.body.type === "formData" && (
@@ -40,6 +64,72 @@ class PublicBodyContainer extends Component {
             handle_change_body_data={this.handleChangeBody.bind(this)}
             original_data={[...this.props.body.value]}
           ></GenericTable>
+        )}
+
+        {this.props.body && this.props.body.type === "JSON" && (
+          <div className="generic-table-container">
+            <div className="public-generic-table-title-container">Body</div>
+            <table className="table table-bordered" id="custom-generic-table">
+              <thead>
+                <tr>
+                  <th className="custom-th"> </th>
+                  <th className="custom-th" id="generic-table-key-cell">
+                    KEY
+                  </th>
+                  <th className="custom-th">VALUE</th>
+                  <th className="custom-th">DATATYPE</th>
+                </tr>
+              </thead>
+              <tbody style={{ border: "none" }}>
+                {this.dataArray.map((e, index) => (
+                  <tr key={index} id="generic-table-row">
+                    <td
+                      className="custom-td"
+                      id="generic-table-key-cell"
+                      style={{ "margin-left": "5px" }}
+                    ></td>
+                    <td className="custom-td">
+                      <input
+                        disabled
+                        name={index + ".key"}
+                        value={this.dataArray[index].key}
+                        onChange={this.handleChange}
+                        type={"text"}
+                        placeholder={
+                          this.dataArray[index].checked === "notApplicable"
+                            ? "Key"
+                            : ""
+                        }
+                        className="form-control"
+                        style={{ border: "none" }}
+                      ></input>
+                    </td>
+                    <td className="custom-td">
+                      <input
+                        name={index + ".value"}
+                        onChange={this.handleChange}
+                        type={"text"}
+                        placeholder="Value"
+                        className="form-control"
+                        style={{ border: "none" }}
+                      />
+                    </td>
+                    <td className="custom-td">
+                      <input
+                        disabled
+                        name={index + ".datatype"}
+                        type={"text"}
+                        value={this.dataArray[index].dataType}
+                        placeholder="DataType"
+                        className="form-control"
+                        style={{ border: "none" }}
+                      />
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         )}
       </div>
     );
