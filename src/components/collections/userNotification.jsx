acb226fccb56a029bff57cdc2d9554121d62fbec
Walk 1 <mohitbangar@walkover.in>
2020-11-24 17:36:27 +0530

    bell icon not to be shown in case of 0 notification

--- src/components/collections/userNotification.jsx
+++ src/components/collections/userNotification.jsx
@@ -1,86 +1,94 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import { Dropdown } from 'react-bootstrap'
-import authService from '../auth/authService'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Dropdown } from "react-bootstrap";
+import authService from "../auth/authService";
+import vectorIcon from "../../assets/icons/Vector.svg";
 
 class UserInfo extends Component {
-  constructor (props) {
-    super(props)
-    this.state = { user: { name: '', email: '' } }
+  constructor(props) {
+    super(props);
+    this.state = { user: { name: "", email: "" } };
   }
 
-  componentDidMount () {
+  componentDidMount() {
     if (authService.getCurrentUser()) {
-      const user = {}
-      const currentUser = authService.getCurrentUser()
-      user.name = currentUser.first_name + currentUser.last_name
-      user.email = currentUser.email
-      this.setState({ user })
+      const user = {};
+      const currentUser = authService.getCurrentUser();
+      user.name = currentUser.first_name + currentUser.last_name;
+      user.email = currentUser.email;
+      this.setState({ user });
     }
   }
 
-  getFirstPublicCollection () {
-    const allCollections = this.props.get_public_collections()
+  getFirstPublicCollection() {
+    const allCollections = this.props.get_public_collections();
 
-    let firstCollection = {}
+    let firstCollection = {};
     for (let i = 0; i < allCollections.length; i++) {
-      const collectionId = allCollections[i]
-      const collection = this.props.collections[collectionId]
-      firstCollection = collection
-      break
+      const collectionId = allCollections[i];
+      const collection = this.props.collections[collectionId];
+      firstCollection = collection;
+      break;
     }
-    return firstCollection
+    return firstCollection;
   }
 
-  navigateToPublishDocs () {
-    const collection = this.getFirstPublicCollection()
-    this.props.open_publish_docs(collection)
+  navigateToPublishDocs() {
+    const collection = this.getFirstPublicCollection();
+    this.props.open_publish_docs(collection);
   }
 
-  render () {
+  render() {
+    const notificationCount = this.props.get_notification_count();
+
     return (
       <>
-        <div className='user-notification user-info '>
-          <div className='user-avatar '>
-            <i className='uil uil-user' />
+        <div className="user-notification user-info ">
+          <div className="user-avatar ">
+            <i className="uil uil-user" />
           </div>
-          <div className='user-details'>
-            <div className='user-heading'>
-              <div className='user-name'>
-                {this.state.user.name}
-              </div>
-              {
-                authService.isAdmin() &&
-                  <Dropdown>
-                    <Dropdown.Toggle variant='success' id='dropdown-basic'>
-                      <div class=' user-name notification'>
-                        <span> <i class='fas fa-bell' /></span>
-                        <span class='badge'>{this.props.get_notification_count()}</span>
-                      </div>
-                    </Dropdown.Toggle>
-                    <Dropdown.Menu>
-                      <Dropdown.Item onClick={() => this.navigateToPublishDocs()}>Hosted API</Dropdown.Item>
-                      <Dropdown.Item>
-                        <li>
-                          <Link to='/logout'>
-                            Sign out
-                          </Link>
-                        </li>
-                      </Dropdown.Item>
-                    </Dropdown.Menu>
-                  </Dropdown>
-              }
-              {
-                !authService.isAdmin() &&
-                  <Link to='/logout'>Sign out</Link>
-
-              }
+          <div className="user-details">
+            <div className="user-heading">
+              <div className="user-name">{this.state.user.name}</div>
+              {authService.isAdmin() && (
+                <Dropdown>
+                  <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    <div class=" user-name notification">
+                      {notificationCount && (
+                        <>
+                          <span>
+                            {" "}
+                            <i class="fas fa-bell" />
+                          </span>
+                          <span class="badge">{notificationCount}</span>
+                        </>
+                      )}
+                      <img
+                        src={vectorIcon}
+                        className="notification-icon"
+                        alt=""
+                      />
+                    </div>
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                    <Dropdown.Item onClick={() => this.navigateToPublishDocs()}>
+                      Hosted API
+                    </Dropdown.Item>
+                    <Dropdown.Item>
+                      <li>
+                        <Link to="/logout">Sign out</Link>
+                      </li>
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              )}
+              {!authService.isAdmin() && <Link to="/logout">Sign out</Link>}
             </div>
           </div>
         </div>
       </>
-    )
+    );
   }
 }
 
-export default UserInfo
+export default UserInfo;
