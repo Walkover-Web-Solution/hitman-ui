eeae9f5f986a1fe831cd017637aed3ca86b26c11
Umasankar Cherukuri <umasankar@walkover.in>
2020-11-27 08:50:27 +0530

    fix: redirect to relevant page after checking domain

--- src/components/landing/landing.jsx
+++ src/components/landing/landing.jsx
@@ -1,16 +1,43 @@
 import React, { Component } from 'react'
-
+import collectionsApiService from '../collections/collectionsApiService'
+import LoadingScreen from 'react-loading-screen'
 class Landing extends Component {
   constructor (props) {
     super(props)
     this.state = {}
   }
 
+  async redirectToRelevantPage () {
+    const domainsList = process.env.REACT_APP_DOMAINS_LIST ? process.env.REACT_APP_DOMAINS_LIST.split(',') : []
+    const currentDomain = window.location.href.split('/')[2]
+    if (!domainsList.includes(currentDomain)) {
+      const { data: clientCollection } = await collectionsApiService.getCollectionsByCustomDomain(currentDomain)
+      console.log(clientCollection)
+      if (Object.keys(clientCollection) && Object.keys(clientCollection)[0]) {
+        const clientCollectionId = Object.keys(clientCollection)[0]
+        this.props.history.push({ pathname: `/p/${clientCollectionId}` })
+      } else {
+        this.props.history.push({ pathname: '/p/error' })
+      }
+    } else {
+      this.props.history.push({
+        pathname: '/dashboard'
+      })
+    }
+  }
+
   render () {
-    this.props.history.push({
-      pathname: '/dashboard'
-    })
-    return (<div />)
+    this.redirectToRelevantPage()
+    return (
+      <LoadingScreen
+        loading='true'
+        bgColor='#f1f1f1'
+        spinnerColor='#9ee5f8'
+        textColor='#676767'
+        logoSrc='/static/media/hitman.8a9ab55b.svg'
+        text="Hold on! You're being redirected to the doc."
+      />
+    )
   }
 }
 
