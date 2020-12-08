a255615ff42a1947d472f1415582ed0424058184
Priyush Panwar <priyush@walkover.in>
2020-12-08 12:57:07 +0530

    remove position in pages

--- src/components/pages/pageForm.jsx
+++ src/components/pages/pageForm.jsx
@@ -46,33 +46,33 @@ class PageForm extends Form {
     if (this.props.title === 'Add New Version Page') {
       const versionId = this.props.selectedVersion.id
       const data = { ...this.state.data }
-      data.position = this.extractPosition()
+      // data.position = this.extractPosition()
       const newPage = { ...data, requestId: shortid.generate() }
       this.props.add_page(versionId, newPage)
     }
   }
 
-  extractPosition () {
-    let count = -1
-    for (let i = 0; i < Object.keys(this.props.pages).length; i++) {
-      if (
-        this.props.selectedGroup &&
-        this.props.selectedVersion &&
-        this.props.selectedGroup.id ===
-        this.props.pages[Object.keys(this.props.pages)[i]].groupId
-      ) {
-        count = count + 1
-      } else if (
-        this.props.selectedVersion &&
-        this.props.pages[Object.keys(this.props.pages)[i]].groupId === null &&
-        this.props.selectedVersion.id ===
-        this.props.pages[Object.keys(this.props.pages)[i]].versionId
-      ) {
-        count = count + 1
-      }
-    }
-    return count + 1
-  }
+  // extractPosition () {
+  //   let count = -1
+  //   for (let i = 0; i < Object.keys(this.props.pages).length; i++) {
+  //     if (
+  //       this.props.selectedGroup &&
+  //       this.props.selectedVersion &&
+  //       this.props.selectedGroup.id ===
+  //       this.props.pages[Object.keys(this.props.pages)[i]].groupId
+  //     ) {
+  //       count = count + 1
+  //     } else if (
+  //       this.props.selectedVersion &&
+  //       this.props.pages[Object.keys(this.props.pages)[i]].groupId === null &&
+  //       this.props.selectedVersion.id ===
+  //       this.props.pages[Object.keys(this.props.pages)[i]].versionId
+  //     ) {
+  //       count = count + 1
+  //     }
+  //   }
+  //   return count + 1
+  // }
 
   render () {
     return (
