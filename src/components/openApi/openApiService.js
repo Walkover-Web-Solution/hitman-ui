7fd7a1843a838e2a1083bae20fe1dc8f4d77351d
bhunesh2k14 <bhuwneshwar@walkover.in>
2020-12-18 18:15:15 +0530

    fix: import routes

--- src/components/openApi/openApiService.js
+++ src/components/openApi/openApiService.js
@@ -1,13 +1,18 @@
 import http from '../../services/httpService'
+import authService from '../auth/authService'
 
 const apiUrl = process.env.REACT_APP_API_URL
 
 export function importApi (openApiObject) {
-  return http.post(`${apiUrl}/import/openApi/`, openApiObject)
+  const user = authService.getCurrentUser()
+  const userId = user.identifier
+  return http.post(`${apiUrl}/import/openApi/${userId}`, openApiObject)
 }
 
 export function importPostmanCollection (collection, website) {
-  return http.post(`${apiUrl}/import/postman?website=${website}`, collection)
+  const user = authService.getCurrentUser()
+  const userId = user.identifier
+  return http.post(`${apiUrl}/import/postman/${userId}?website=${website}`, collection)
 }
 
 export default {
