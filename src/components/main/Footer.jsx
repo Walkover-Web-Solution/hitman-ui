cd65210b29acc34f2665649c68550801f9d2afcd
bhunesh2k14 <bhuwneshwar@walkover.in>
2020-12-18 13:22:27 +0530

    feat: add footer

---
+++ src/components/main/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const footerMessage = 'Love developer. ♥ ViaSocket'
+
+function Footer () {
+  return (
+    <div>{footerMessage}</div>
+  )
+}
+
+export default Footer
