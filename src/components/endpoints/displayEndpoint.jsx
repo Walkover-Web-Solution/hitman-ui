eab2c5d51c3c0a8cfde031d2de123e3537817892
Deepanshu <deepanshuwaiker2512@gmail.com>
2021-01-13 19:06:51 +0530

    Revert commits

--- src/components/endpoints/displayEndpoint.jsx
+++ src/components/endpoints/displayEndpoint.jsx
@@ -35,8 +35,6 @@ import indexedDbService from '../indexedDb/indexedDbService'
 import Authorization from './displayAuthorization'
 import LoginSignupModal from '../main/loginSignupModal'
 import PublicSampleResponse from './publicSampleResponse'
-import Notes from './notes'
-import ReactHtmlParser from 'react-html-parser'
 const shortid = require('shortid')
 
 const status = require('http-status')
@@ -1805,7 +1803,7 @@ class DisplayEndpoint extends Component {
               {!isDashboardRoute(this.props) && (
                 <div className='endpoint-name-container'>
                   {!isDashboardRoute(this.props, true) && <h1 className='endpoint-title'>{this.state.data?.name || ''}</h1>}
-                  <p>{ReactHtmlParser(this.state.endpoint?.description) || ''}</p>
+                  <p>{this.state.endpoint?.description || ''}</p>
                 </div>
               )}
             </div>
@@ -2205,13 +2203,6 @@ class DisplayEndpoint extends Component {
                       </button>
                     </div>
                   )
-                }
-                {
-                   !isDashboardRoute(this.props) &&
-                     <Notes
-                       {...this.props}
-                       note={this.props.endpoints[this.props.endpointId]?.notes || ''}
-                     />
                 }
                 {
                   this.displayResponse()
