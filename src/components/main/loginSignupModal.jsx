e74a9c01619d0b1900de4b413f6905c5fe4f83c4
Rohit Nandan Dubey <dubeyrohitnandan@gmail.com>
2021-02-06 22:24:43 +0530

    feat:login/signup popup content changes

--- src/components/main/loginSignupModal.jsx
+++ src/components/main/loginSignupModal.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { Modal } from 'react-bootstrap'
 import notLoggedIn from '../../assets/icons/notLoggedIn.svg'
+import './loginSignupModal.scss'
 
 function LoginSignupModal (props) {
   const redirectionUrl = process.env.REACT_APP_UI_URL + '/login'
@@ -50,6 +51,15 @@ function LoginSignupModal (props) {
             data-app-logo-url='https://hitman.app/wp-content/uploads/2020/12/123.png'
             signup_uri={redirectionUrl + '?signup=true'}
           />
+        </div>
+        <div className='list'>
+          <div className='heading'>Logged in user can:</div>
+          <ol>
+            <li>Host Public API documentation</li>
+            <li>Create/Manage Environment</li>
+            <li>Create/Share internal API documentation</li>
+            <li>Get Access to our Marketplace</li>
+          </ol>
         </div>
       </Modal.Body>
     </Modal>
