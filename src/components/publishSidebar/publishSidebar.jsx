eafff8d7a0d30d45180511ddbc00af396c016130
Deepanshu <deepanshuwaiker2512@gmail.com>
2021-04-09 13:14:08 +0530

    fix: data from state for single collection

--- src/components/publishSidebar/publishSidebar.jsx
+++ src/components/publishSidebar/publishSidebar.jsx
@@ -7,6 +7,7 @@ import './publishSidebar.scss'
 import { isAdmin } from '../auth/authService'
 import { ReactComponent as DownChevron } from '../../assets/icons/downChevron.svg'
 import { ReactComponent as GlobeIcon } from '../../assets/icons/globe-icon.svg'
+import extractCollectionInfoService from '../publishDocs/extractCollectionInfoService'
 
 const mapStateToProps = (state) => {
   return {
@@ -38,28 +39,47 @@ export class PublishSidebar extends Component {
       groupData: {},
       versionData: {},
       checkedData: {},
-      versionsToggle: {}
+      versionsToggle: {},
+      versions: {},
+      groups: {},
+      pages: {},
+      endpoints: {}
     }
   }
 
   componentDidMount () {
     const selectedCollectionId = this.props.collectionId
     if (selectedCollectionId) {
-      this.setState({ selectedCollectionId })
+      this.setState({ selectedCollectionId }, () => {
+        this.getAllData()
+      })
     }
-    this.makeVersionData()
-    this.makeGroupData()
+    // this.makeVersionData()
+    // this.makeGroupData()
   }
 
   componentDidUpdate (prevProps, prevState) {
     if (prevProps.endpoints !== this.props.endpoints || prevProps.groups !== this.props.groups || prevProps.pages !== this.props.pages || prevProps.versions !== this.props.versions) {
-      this.makeVersionData()
-      this.makeGroupData()
+      // this.makeVersionData()
+      // this.makeGroupData()
+      this.getAllData()
     }
 
     if (this.state.selectedCollectionId !== prevState.selectedCollectionId) {
       this.setState({ checkedData: {} })
     }
+  }
+
+  getAllData () {
+    const collectionId = this.state.selectedCollectionId || this.props.collectionId
+    const versions = extractCollectionInfoService.extractVersionsFromCollectionId(collectionId, this.props)
+    const groups = extractCollectionInfoService.extractGroupsFromVersions(versions, this.props)
+    const pages = extractCollectionInfoService.extractPagesFromVersions(versions, this.props)
+    const endpoints = extractCollectionInfoService.extractEndpointsFromGroups(groups, this.props)
+    this.setState({ versions, groups, pages, endpoints, selectedCollectionId: collectionId }, () => {
+      this.makeVersionData()
+      this.makeGroupData()
+    })
   }
 
   sendPublishRequest () {
@@ -98,20 +118,20 @@ export class PublishSidebar extends Component {
 
   makeVersionData () {
     const versions = {}
-    Object.values(this.props.versions).forEach((version) => {
+    Object.values(this.state.versions).forEach((version) => {
       versions[version.id] = {
         pages: [],
         groups: []
       }
     })
 
-    Object.values(this.props.pages).forEach((page) => {
+    Object.values(this.state.pages).forEach((page) => {
       if (page.versionId && !page.groupId) {
         versions[page?.versionId].pages = [...versions[page?.versionId].pages, page?.id]
       }
     })
 
-    Object.values(this.props.groups).forEach((group) => {
+    Object.values(this.state.groups).forEach((group) => {
       versions[group?.versionId].groups = [...versions[group?.versionId].groups, group?.id]
     })
 
@@ -120,21 +140,21 @@ export class PublishSidebar extends Component {
 
   makeGroupData () {
     const groups = {}
-    if (Object.keys(this.props.groups).length) {
-      Object.keys(this.props.groups).forEach((groupId) => {
+    if (Object.keys(this.state.groups).length) {
+      Object.keys(this.state.groups).forEach((groupId) => {
         groups[groupId] = {
           pages: [],
           endpoints: []
         }
       })
 
-      Object.values(this.props.pages).forEach((page) => {
+      Object.values(this.state.pages).forEach((page) => {
         if (page?.groupId) {
           groups[page?.groupId].pages = [...groups[page?.groupId].pages, page?.id]
         }
       })
 
-      Object.values(this.props.endpoints).forEach((endpoint) => {
+      Object.values(this.state.endpoints).forEach((endpoint) => {
         if (endpoint?.groupId) {
           groups[endpoint?.groupId].endpoints = [...groups[endpoint?.groupId].endpoints, endpoint?.id]
         }
@@ -172,36 +192,36 @@ export class PublishSidebar extends Component {
   handleBulkCheck (e, itemtype, itemId) {
     const newData = { ...this.state.checkedData }
     if (itemtype === 'endpoint') {
-      const { pages, endpoints } = this.state.groupData[this.props.endpoints[itemId]?.groupId]
+      const { pages, endpoints } = this.state.groupData[this.state.endpoints[itemId]?.groupId]
       if (this.findCheckedItems(endpoints, 'endpoint') && this.findCheckedItems(pages, 'groupPage')) {
-        newData[`check.group.${this.props.endpoints[itemId]?.groupId}`] = true
+        newData[`check.group.${this.state.endpoints[itemId]?.groupId}`] = true
       }
     }
 
     if (itemtype === 'groupPage') {
-      const { pages, endpoints } = this.state.groupData[this.props.pages[itemId]?.groupId]
+      const { pages, endpoints } = this.state.groupData[this.state.pages[itemId]?.groupId]
       if (this.findCheckedItems(pages, 'groupPage') && this.findCheckedItems(endpoints, 'endpoint')) {
-        newData[`check.group.${this.props.pages[itemId]?.groupId}`] = true
+        newData[`check.group.${this.state.pages[itemId]?.groupId}`] = true
       }
     }
 
     if (itemtype === 'versionPage') {
-      const { pages, groups } = this.state.versionData[this.props.pages[itemId]?.versionId]
+      const { pages, groups } = this.state.versionData[this.state.pages[itemId]?.versionId]
       if (this.findCheckedItems(pages, 'versionPage') && this.findCheckedItems(groups, 'group')) {
-        newData[`check.version.${this.props.pages[itemId]?.versionId}`] = true
+        newData[`check.version.${this.state.pages[itemId]?.versionId}`] = true
       }
     }
 
     if (itemtype === 'group') {
-      const { pages, groups } = this.state.versionData[this.props.groups[itemId]?.versionId]
+      const { pages, groups } = this.state.versionData[this.state.groups[itemId]?.versionId]
       if (this.findCheckedItems(groups, 'group') && this.findCheckedItems(pages, 'versionPage')) {
-        newData[`check.version.${this.props.groups[itemId]?.versionId}`] = true
+        newData[`check.version.${this.state.groups[itemId]?.versionId}`] = true
       }
     }
 
     this.setState({ checkedData: { ...this.state.checkedData, ...newData } }, () => {
       if (itemtype === 'endpoint') {
-        this.handleBulkCheck(e, 'group', this.props.endpoints[itemId]?.groupId)
+        this.handleBulkCheck(e, 'group', this.state.endpoints[itemId]?.groupId)
       }
     })
   }
@@ -210,19 +230,19 @@ export class PublishSidebar extends Component {
     if (itemtype === 'groupPage' || itemtype === 'versionPage' || itemtype === 'endpoint') {
       const checkedData = { ...this.state.checkedData }
       const prevChoice = !this.state.checkedData[`check.${e.target.name}`]
-      if (itemtype === 'endpoint' && this.state.checkedData[`check.group.${this.props.endpoints[itemId]?.groupId}`]) {
-        checkedData[`check.group.${this.props.endpoints[itemId]?.groupId}`] = e.target.checked
-        if (this.state.checkedData[`check.version.${this.props.groups[this.props.endpoints[itemId]?.groupId]?.versionId}`]) {
-          checkedData[`check.version.${this.props.groups[this.props.endpoints[itemId]?.groupId]?.versionId}`] = e.target.checked
+      if (itemtype === 'endpoint' && this.state.checkedData[`check.group.${this.state.endpoints[itemId]?.groupId}`]) {
+        checkedData[`check.group.${this.state.endpoints[itemId]?.groupId}`] = e.target.checked
+        if (this.state.checkedData[`check.version.${this.state.groups[this.state.endpoints[itemId]?.groupId]?.versionId}`]) {
+          checkedData[`check.version.${this.state.groups[this.state.endpoints[itemId]?.groupId]?.versionId}`] = e.target.checked
         }
       }
 
-      if (itemtype === 'groupPage' && this.state.checkedData[`check.group.${this.props.pages[itemId]?.groupId}`]) {
-        checkedData[`check.group.${this.props.pages[itemId]?.groupId}`] = e.target.checked
+      if (itemtype === 'groupPage' && this.state.checkedData[`check.group.${this.state.pages[itemId]?.groupId}`]) {
+        checkedData[`check.group.${this.state.pages[itemId]?.groupId}`] = e.target.checked
       }
 
-      if (itemtype === 'versionPage' && this.state.checkedData[`check.version.${this.props.pages[itemId]?.versionId}`]) {
-        checkedData[`check.version.${this.props.pages[itemId]?.versionId}`] = e.target.checked
+      if (itemtype === 'versionPage' && this.state.checkedData[`check.version.${this.state.pages[itemId]?.versionId}`]) {
+        checkedData[`check.version.${this.state.pages[itemId]?.versionId}`] = e.target.checked
       }
 
       checkedData[`check.${e.target.name}`] = prevChoice
@@ -234,8 +254,8 @@ export class PublishSidebar extends Component {
     if (itemtype === 'group') {
       let checkedGroupData = { ...this.state.checkedData }
 
-      if (this.state.checkedData[`check.version.${this.props.groups[itemId]?.versionId}`]) {
-        checkedGroupData[`check.version.${this.props.groups[itemId]?.versionId}`] = false
+      if (this.state.checkedData[`check.version.${this.state.groups[itemId]?.versionId}`]) {
+        checkedGroupData[`check.version.${this.state.groups[itemId]?.versionId}`] = false
       }
 
       checkedGroupData = { ...checkedGroupData, ...this.handleGroupCheckbox(itemId, e) }
