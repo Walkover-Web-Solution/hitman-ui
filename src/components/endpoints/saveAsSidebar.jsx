74dced7a5271b11032fcb195f283fdecc1170b00
Deepanshu <deepanshuwaiker2512@gmail.com>
2021-04-26 11:57:58 +0530

    fix: + replaced with add

--- src/components/endpoints/saveAsSidebar.jsx
+++ src/components/endpoints/saveAsSidebar.jsx
@@ -292,7 +292,7 @@ class SaveAsSidebar extends Form {
                               this.openAddModal()
                             }}
                           >
-                            <svg width='18' height='18' viewBox='0 0 18 18' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M9 3.75V14.25' stroke='#E98A36' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round' /><path d='M3.75 9H14.25' stroke='#E98A36' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round' /></svg>
+                            + Add
                           </button>
                         </div>
                         )
@@ -315,7 +315,7 @@ class SaveAsSidebar extends Form {
                                 this.openAddModal()
                               }}
                             >
-                              <svg width='18' height='18' viewBox='0 0 18 18' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M9 3.75V14.25' stroke='#E98A36' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round' /><path d='M3.75 9H14.25' stroke='#E98A36' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round' /></svg>
+                              + Add
                             </button>
                           </div>
                           )
