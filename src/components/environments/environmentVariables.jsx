ff231983fee1841840b46a2a7e4578d4d4c8288e
sourabh04 <sourabh@walkover.in>
2021-04-27 10:09:09 +0530

    Debug:env var error

--- src/components/environments/environmentVariables.jsx
+++ src/components/environments/environmentVariables.jsx
@@ -138,6 +138,7 @@ class EnvironmentVariables extends Component {
   };
 
   handleChange = (e) => {
+    console.log('currentTarget 1',e.currentTarget)
     const name = e.currentTarget.name.split('.')
     const lastIndex = this.state.originalVariableNames.length - 1
 
@@ -153,9 +154,9 @@ class EnvironmentVariables extends Component {
         e.currentTarget.value
       data = { environment }
     }
-
+    console.log('currentTarget 2 ',e.currentTarget)
     this.setState(data, () => {
-      if (name[0] === lastIndex.toString()) {
+      if (name[0] === lastIndex.toString() && e?.currentTarget) {
         this.handleAdd(e.currentTarget.value)
       }
     })
