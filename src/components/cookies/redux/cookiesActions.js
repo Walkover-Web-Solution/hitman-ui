da94f00e83dfbe9b4200dd086a995dc3fcdcf8e4
Deepanshu <deepanshuwaiker2512@gmail.com>
2021-06-09 16:19:12 +0530

    feat: cookies add and fetch all action

---
+++ src/components/cookies/redux/cookiesActions.js
@@ -0,0 +1,64 @@
+import cookiesApiService from '../cookiesApiService'
+import cookiesActionTypes from './cookiesActionTypes'
+
+export const fetchAllCookies = (data) =>{
+  return(dispatch) =>{
+    dispatch(addDomainRequest(data))
+    cookiesApiService
+      .getAllCookies()
+      .then((response)=>{
+        dispatch(onCookiesFetched(response.data))
+      }).catch((error)=>{
+        dispatch(onCookiesFetchedError(error.response ? error.response.data : error))
+      })
+  }
+}
+
+export const onCookiesFetched = (cookies) => {
+  return {
+    type: cookiesActionTypes.ON_COOKIES_FETCHED,
+    cookies
+  }
+}
+
+export const onCookiesFetchedError = (error) => {
+  return {
+    type: cookiesActionTypes.ON_COOKIES_FETCHED_ERROR,
+    error
+  }
+}
+
+export const addCookieDomain = (data) =>{
+  return(dispatch) =>{
+    dispatch(addDomainRequest(data))
+    cookiesApiService
+      .addDomain(data)
+      .then((response)=>{
+        dispatch(onDomainAdded(response.data,data))
+      }).catch((error)=>{
+        dispatch(onDomainAddedError(error.response ? error.response.data : error, data))
+      })
+  }
+}
+
+export const addDomainRequest = (newDomain) => {
+  return {
+    type: cookiesActionTypes.ADD_DOMAIN_REQUEST,
+    newDomain
+  }
+}
+
+export const onDomainAdded = (domain) => {
+  return {
+    type: cookiesActionTypes.ON_DOMAIN_ADDED,
+    domain
+  }
+}
+
+export const onDomainAddedError = (error, domain) => {
+  return {
+    type: cookiesActionTypes.ON_DOMAIN_ADDED_ERROR,
+    domain,
+    error
+  }
+}
--- src/components/cookies/redux/cookiesReducer.js
+++ src/components/cookies/redux/cookiesReducer.js
@@ -1,10 +1,35 @@
-// import cookiesActionTypes from './cookiesActionTypes'
-// import { toast } from 'react-toastify'
+import cookiesActionTypes from './cookiesActionTypes'
+import { toast } from 'react-toastify'
 
 const initialState = {}
 
 function cookiesReducer (state = initialState, action) {
+  let cookies = {}
   switch (action.type) {
+    case cookiesActionTypes.ON_COOKIES_FETCHED:
+      return {...action.cookies}
+
+    case cookiesActionTypes.ON_COOKIES_FETCHED_ERROR:
+      toast.error(action.error)
+      return state;
+
+    case cookiesActionTypes.ADD_DOMAIN_REQUEST:
+      cookies = {...state}
+      cookies[action.newDomain.requestId] = {...action.newDomain}
+      return cookies;
+
+    case cookiesActionTypes.ON_DOMAIN_ADDED:
+      cookies = {...state}
+      delete cookies[action.domain.requestId]
+      cookies[action.domain.id] = {...action.domain}
+      return cookies;
+
+    case cookiesActionTypes.ON_DOMAIN_ADDED_ERROR:
+      toast.error(action.error)
+      cookies = {...state}
+      delete cookies[action.domain.requestId]
+      return cookies;
+
     default:
       return state
   }
