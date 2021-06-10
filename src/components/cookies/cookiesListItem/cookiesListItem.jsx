1319324a7a212e0c124af13d3e2ebf468d09b516
Deepanshu <deepanshuwaiker2512@gmail.com>
2021-06-10 17:29:21 +0530

    WIP: cookie edit and save

--- src/components/cookies/cookiesListItem/cookiesListItem.jsx
+++ src/components/cookies/cookiesListItem/cookiesListItem.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 import moment from 'moment'
+import './cookiesListItem.scss'
 class CookiesListItem extends Component {
   constructor (props) {
     super(props)
 
     this.state = {
       addCookie: false,
-      updateCookie: null,
+      updateCookie: {
+        key: '',
+        value: ''
+      },
       currentDomain: {}
     }
   }
@@ -27,15 +31,53 @@ class CookiesListItem extends Component {
     domain.cookies[newCookie.key] = newCookie.value
     this.props.update_cookies(domain)
     // console.log(newCookie.key)
-    this.setState({ updateCookie: newCookie.key })
+    this.setState({ updateCookie: newCookie })
+  }
+
+  handleCookieEdit (e) {
+    const cookieValue = e.target.value
+    this.setState({ updateCookie: { ...this.state.updateCookie, value: cookieValue } })
+  }
+
+  saveCookie () {
+    const currentDomain = { ...this.state.currentDomain }
+    const currentCookie = this.state.updateCookie
+    let updateCookie = {}
+    let cookieArray = []
+    let cookieName = ''
+
+    if (currentDomain.cookies[currentCookie.key] === currentCookie.value) {
+      updateCookie = {}
+    } else if (currentCookie.value.trim()) {
+      cookieArray = currentCookie.value.split(';')
+      cookieName = cookieArray[0].split('=')[0]
+
+      delete currentDomain.cookies[currentCookie.key]
+
+      // if(cookieArray[0]){
+      //   if(!cookieArray[0].split('=')[1]){
+      //     cookieArray[0] = cookieArray[0].split('=')[0] + '='
+      //   }
+      //   if(cookieArray[1]){
+      //     if(cookieArray[1].split('=')[0] !=='Path' && cookieArray[2].split('=')[0] !=="Expires"){
+      //       cookieArray[1] = 'Path=/'
+      //       currentDomain.cookies[cookieName] = cookieArray.join("; ")
+      //     }else{
+      currentDomain.cookies[cookieName] = currentCookie.value
+      //   }
+      // }
+      this.props.update_cookies(currentDomain)
+      // }
+    }
+    this.setState({ updateCookie })
   }
 
   renderCookiesList () {
     return (
       Object.keys(this.state.currentDomain.cookies || {}).map((name, index) => (
-        <div key={index} onClick={() => this.setState({ updateCookie: name })}>
-          {name}
-          {name === this.state.updateCookie && this.renderCookieArea(this.state.currentDomain.cookies[name])}
+        <div className='cookie-item' key={index}>
+          <div onClick={() => this.setState({ updateCookie: { key: name, value: this.state.currentDomain.cookies[name] } })}>{name}</div>
+          {name === this.state.updateCookie.key && this.renderCookieArea()}
         </div>
       ))
     )
@@ -54,23 +96,21 @@ class CookiesListItem extends Component {
     }
   }
 
-  renderCookieArea (cookie) {
-    console.log('cookie', cookie)
+  renderCookieArea () {
     return (
       <>
         <textarea
           className='form-control custom-input'
           rows='2'
-        //  onChange={this.handleChange}
+          onChange={(e) => this.handleCookieEdit(e)}
           id='update-cookie'
-        //  error={errors[name]}
-        //  name={name}
-          value={cookie}
-        //  placeholder={placeholder}
+          //  error={errors[name]}
+          //  name={name}
+          value={this.state.updateCookie.value}
         />
-        <div className='d-flex'>
-          <span>cancel</span>
-          <button className='btn btn-primary'>save</button>
+        <div className='text-right mt-2'>
+          <span className='mr-3' onClick={() => this.setState({ updateCookie: {} })}>cancel</span>
+          <button className='btn btn-primary' onClick={() => this.saveCookie()}>save</button>
         </div>
       </>
     )
@@ -80,14 +120,14 @@ class CookiesListItem extends Component {
     return (
       <div>
         <div onClick={() => this.props.changeModalTab(1)}>{'< back'}</div>
-        <div className='d-flex justify-content-between'>
+        <div className='d-flex justify-content-between align-items-center'>
           <div>{this.state.currentDomain.domain}</div>
-          <div>
-            <span>{`${Object.entries(this.state.currentDomain.cookies || {}).length} cookies`}</span>
+          <div className='d-flex justify-content-between align-items-center'>
+            <div className='mr-3'>{`${Object.entries(this.state.currentDomain.cookies || {}).length} cookies`}</div>
             <button className='btn btn-primary' onClick={() => this.handleAddCookie()}>Add Cookie</button>
           </div>
         </div>
-        {this.renderCookiesList()}
+        <div className='mt-3'>{this.renderCookiesList()}</div>
       </div>
     )
   }
