fa69e29607976ae294a21c026367617a146656c3
Rohit Dubey <dubeyrohitnandan@gmail.com>
2021-07-05 13:17:24 +0530

    feat: widget code uncommented and user-data atribute added in script

--- src/services/widgetService.js
+++ src/services/widgetService.js
@@ -1,13 +1,14 @@
-// const widgetURL = process.env.REACT_APP_STEVE_WIDGET_URL
-// const projectId = process.env.REACT_APP_STEVE_PROJECT_ID
-// const authkey = process.env.REACT_APP_STEVE_AUTHKEY
-// const widgetId = process.env.REACT_APP_STEVE_WIDGET_ID
+
+const widgetURL = process.env.REACT_APP_STEVE_WIDGET_URL
+const projectId = process.env.REACT_APP_STEVE_PROJECT_ID
+const authkey = process.env.REACT_APP_STEVE_AUTHKEY
+const widgetId = process.env.REACT_APP_STEVE_WIDGET_ID
 
 export function moveToNextStep (currentStepNo) {
-  // if (window.isUserOnboardingComplete && window.isUserOnboardingComplete() === false) {
-  //   const steveEvent = new window.CustomEvent('steveOnboarding', { detail: { doneStep: currentStepNo } })
-  //   window.dispatchEvent(steveEvent)
-  // }
+  if (window.isUserOnboardingComplete && window.isUserOnboardingComplete() === false) {
+    const steveEvent = new window.CustomEvent('steveOnboarding', { detail: { doneStep: currentStepNo } })
+    window.dispatchEvent(steveEvent)
+  }
 }
 
 export function onOnboardingCompleted () {
@@ -15,23 +16,40 @@ export function onOnboardingCompleted () {
   window.dispatchEvent(steveEvent)
 }
 
+function getUserName (profile) {
+  const firstName = profile?.first_name || ''
+  const lastName = profile?.last_name || ''
+  const name = firstName + ' ' + lastName
+  return name?.trim()
+}
+
 export function loadWidget () {
-  // const user = window.localStorage.getItem('profile')
-  // const identifier = JSON.parse(user).identifier
-  // const onboardingWgt = document.getElementById('onboarding-wgt-script')
-  // if (!onboardingWgt && identifier) {
-  //   const script = document.createElement('script')
-  //   script.src = widgetURL
-  //   script.id = 'onboarding-wgt-script'
-  //   script.type = 'text/javascript'
-  //   script.setAttribute('project-id', projectId)
-  //   script.setAttribute('user-id', identifier)
-  //   script.setAttribute('authkey', authkey)
-  //   script.setAttribute('widget-id', widgetId)
-  //   document.body.appendChild(script)
-  //   script.onload = () => {
-  //   }
-  // }
+  let user = window.localStorage.getItem('profile')
+  user = JSON.parse(user)
+  const identifier = user.identifier
+  const onboardingWgt = document.getElementById('onboarding-wgt-script')
+  let userData = {}
+  try {
+    const userName = getUserName(user)
+    userData = {
+      name: userName,
+      email: user.email
+    }
+  } catch { }
+  if (!onboardingWgt && identifier) {
+    const script = document.createElement('script')
+    script.src = widgetURL
+    script.id = 'onboarding-wgt-script'
+    script.type = 'text/javascript'
+    script.setAttribute('project-id', projectId)
+    script.setAttribute('user-id', identifier)
+    script.setAttribute('authkey', authkey)
+    script.setAttribute('widget-id', widgetId)
+    script.setAttribute('user-data', JSON.stringify(userData))
+    document.body.appendChild(script)
+    script.onload = () => {
+    }
+  }
 }
 
 export default {
