a83e41a2d6b49fa06e192955fa15f53257ce8bfd
bhunesh2k14 <bhuwneshwar@walkover.in>
2021-07-31 18:08:32 +0530

    fix: enabling steve widget and disabling feedio widget

--- src/services/widgetService.js
+++ src/services/widgetService.js
@@ -1,58 +1,59 @@
 
-// const widgetURL = process.env.REACT_APP_STEVE_WIDGET_URL
-// const projectId = process.env.REACT_APP_STEVE_PROJECT_ID
-// const authkey = process.env.REACT_APP_STEVE_AUTHKEY
-// const widgetId = process.env.REACT_APP_STEVE_WIDGET_ID
-// const FIRST_JULY_2021 = new Date('2021-07-01').valueOf()
+const widgetURL = process.env.REACT_APP_STEVE_WIDGET_URL
+const projectId = process.env.REACT_APP_STEVE_PROJECT_ID
+const authkey = process.env.REACT_APP_STEVE_AUTHKEY
+const widgetId = process.env.REACT_APP_STEVE_WIDGET_ID
+const FIRST_JULY_2021 = new Date('2021-07-01').valueOf()
 
 export function moveToNextStep (currentStepNo) {
-  // if (window.isUserOnboardingComplete && window.isUserOnboardingComplete() === false) {
-  //   const steveEvent = new window.CustomEvent('steveOnboarding', { detail: { doneStep: currentStepNo } })
-  //   window.dispatchEvent(steveEvent)
-  // }
+  if (window.isUserOnboardingComplete && window.isUserOnboardingComplete() === false) {
+    const steveEvent = new window.CustomEvent('steveOnboarding', { detail: { doneStep: currentStepNo } })
+    window.dispatchEvent(steveEvent)
+  }
 }
 
 export function onOnboardingCompleted () {
-  // const steveEvent = new window.CustomEvent('steveOnboarding', { detail: { isOnboardingComplete: true } })
-  // window.dispatchEvent(steveEvent)
+  const steveEvent = new window.CustomEvent('steveOnboarding', { detail: { isOnboardingComplete: true } })
+  window.dispatchEvent(steveEvent)
 }
 
-// function getUserName (profile) {
-//   const firstName = profile?.first_name || ''
-//   const lastName = profile?.last_name || ''
-//   const name = firstName + ' ' + lastName
-//   return name?.trim()
-// }
+function getUserName (profile) {
+  const firstName = profile?.first_name || ''
+  const lastName = profile?.last_name || ''
+  const name = firstName + ' ' + lastName
+  return name?.trim()
+}
 
 export function loadWidget () {
-  // let user = window.localStorage.getItem('profile')
-  // user = JSON.parse(user)
-  // const userCreatedAt = new Date(user?.created_at)?.valueOf()
-  // const identifier = user.identifier
-  // const onboardingWgt = document.getElementById('onboarding-wgt-script')
-  // let userData = {}
-  // try {
-  //   const userName = getUserName(user)
-  //   userData = {
-  //     name: userName,
-  //     email: user.email,
-  //     userId: identifier,
-  //     isOnboardingComplete: userCreatedAt < FIRST_JULY_2021
-  //   }
-  // } catch { }
-  // if (!onboardingWgt && identifier) {
-  //   const script = document.createElement('script')
-  //   script.src = widgetURL
-  //   script.id = 'onboarding-wgt-script'
-  //   script.type = 'text/javascript'
-  //   script.setAttribute('project-id', projectId)
-  //   script.setAttribute('authkey', authkey)
-  //   script.setAttribute('widget-id', widgetId)
-  //   script.setAttribute('user-data', JSON.stringify(userData))
-  //   document.body.appendChild(script)
-  //   script.onload = () => {
-  //   }
-  // }
+  let user = window.localStorage.getItem('profile')
+  user = JSON.parse(user)
+  const userCreatedAt = new Date(user?.created_at)?.valueOf()
+  const identifier = user.identifier
+  const onboardingWgt = document.getElementById('onboarding-wgt-script')
+  let userData = {}
+  try {
+    const userName = getUserName(user)
+    userData = {
+      name: userName,
+      email: user.email,
+      userId: identifier,
+      isOnboardingComplete: userCreatedAt < FIRST_JULY_2021
+    }
+  } catch { }
+  if (!onboardingWgt && identifier) {
+    const script = document.createElement('script')
+    script.src = widgetURL
+    script.id = 'onboarding-wgt-script'
+    script.type = 'text/javascript'
+    script.setAttribute('project-id', projectId)
+    script.setAttribute('authkey', authkey)
+    script.setAttribute('widget-id', widgetId)
+    script.setAttribute('user-data', JSON.stringify(userData))
+    script.setAttribute('debug', true)
+    document.body.appendChild(script)
+    script.onload = () => {
+    }
+  }
 }
 
 export default {
