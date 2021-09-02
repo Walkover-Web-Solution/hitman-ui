490b0abfeb12b1517de9bbff5adef2e6c5316775
bhunesh2k14 <bhuwneshwar@walkover.in>
2021-09-02 18:42:57 +0530

    feat: add URL validation Regex for Joi validations

---
+++ src/components/common/constants.js
@@ -0,0 +1,6 @@
+/** REGEX to match URL Validations */
+export const URL_VALIDATION_REGEX = /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{2,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)$/i
+
+export default {
+  URL_VALIDATION_REGEX
+}
