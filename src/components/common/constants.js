4b3bc8b1e991e95ecbefa5f3226725a85eb01959
Punit <punitgurnani8@gmail.com>
2021-10-05 22:27:16 +0530

    fix: Version endpoint can be localhost

--- src/components/common/constants.js
+++ src/components/common/constants.js
@@ -1,5 +1,5 @@
 /** REGEX to match URL Validations */
-export const URL_VALIDATION_REGEX = /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{2,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)$/i
+export const URL_VALIDATION_REGEX = /^https?:\/\/(localhost|(?!.{256})(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+((?:[a-z]{1,63})|xn--[a-z0-9]{1,59}))\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)$/i
 
 /** REGEX to match Origin part of URL */
 export const URL_ORIGIN_VALIDATION_REGEX = /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{2,6}$/i
