909a86ca8b54c9ded378ad9ced1904dd08cf4bff
kunal2899 <kunaljain649@gmail.com>
2022-03-04 16:47:23 +0530

    feat: resizable sidebar

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -31,6 +31,7 @@ import DesktopAppDownloadModal from './desktopAppPrompt'
 import { sendAmplitudeData } from '../../services/amplitude'
 import UpdateStatus from './updateStatus'
 import { isValidDomain } from '../common/utility'
+import SplitPane from 'react-split-pane';
 
 const mapDispatchToProps = (dispatch) => {
   return {
@@ -180,20 +181,22 @@ class Main extends Component {
           <DesktopAppDownloadModal history={this.props.history} location={this.props.location} match={this.props.match} />
           <OnlineSatus fetchFromBackend={this.fetchFromBackend.bind(this)} isIdbUpdated={this.isIdbUpdated.bind(this)} />
           <div className='main-panel-wrapper'>
-            <SideBar
-              {...this.props}
-              tabs={[...this.state.tabs]}
-              set_tabs={this.setTabs.bind(this)}
-              default_tab_index={this.state.defaultTabIndex}
-            />
-            {this.props.location.pathname.split('/')[4] === 'publish'
-              ? <PublishDocs {...this.props} />
-              : <ContentPanel
+            <SplitPane split='vertical' className="split-sidebar">
+                <SideBar
                   {...this.props}
-                  set_environment={this.setEnvironment.bind(this)}
+                  tabs={[...this.state.tabs]}
                   set_tabs={this.setTabs.bind(this)}
                   default_tab_index={this.state.defaultTabIndex}
-                />}
+                />
+                {this.props.location.pathname.split('/')[4] === 'publish'
+                  ? <PublishDocs {...this.props} />
+                  : <ContentPanel
+                      {...this.props}
+                      set_environment={this.setEnvironment.bind(this)}
+                      set_tabs={this.setTabs.bind(this)}
+                      default_tab_index={this.state.defaultTabIndex}
+                    />}
+            </SplitPane>
           </div>
           <UpdateStatus />
         </div>
