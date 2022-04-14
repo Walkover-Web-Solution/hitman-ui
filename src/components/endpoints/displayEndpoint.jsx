2341dfc9f003bbc2b7a37c2c126e85b62841fbd9
deepanshu <deepanshuwaiker2512@gmail.com>
2022-04-14 10:49:20 +0530

    fix: ssl toggle name

--- src/components/endpoints/displayEndpoint.jsx
+++ src/components/endpoints/displayEndpoint.jsx
@@ -2817,7 +2817,7 @@ class DisplayEndpoint extends Component {
                       </div>
                     )
                 }
-                    {isElectron() && <div onClick={() => this.setSslMode()}>Invalid SSL certificate blocking{this.state.sslMode ? ' on' : ' off'}</div>}
+                    {isElectron() && <div onClick={() => this.setSslMode()}>SSL verification{this.state.sslMode ? ' enabled' : ' disabled'}</div>}
                     <div
                       className={
                     this.isDashboardAndTestingView()
