09261843bc3e1525c22ea7ac24415f9b8fb8223a
Prince <prince@whozzat.com>
2023-11-13 21:14:29 +0530

    changed react-html-parser to html-react-parser and made code changes accordingly

--- src/components/collections/displayCollection.jsx
+++ src/components/collections/displayCollection.jsx
@@ -1,17 +1,17 @@
 import React, { Component } from 'react'
 import store from '../../store/store'
-import ReactHtmlParser from 'react-html-parser'
+import ReactHtmlParser from 'html-react-parser'
 
 class DisplayCollection extends Component {
   constructor (props) {
     super(props)
-    this.state = {
+        this.state = {
       description: ''
     }
   }
 
   async componentDidMount () {
-    if (!this.props.location.collection) {
+        if (!this.props.location.collection) {
       const collectionId = this.props.location.pathname.split('/')[2]
       this.fetchCollection(collectionId)
       store.subscribe(() => {
