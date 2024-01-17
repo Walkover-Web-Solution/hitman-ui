fb129734b0d267e8fa957253e2887fa855f68314
Rashiik <rashikhandelwal269@gmail.com>
2024-01-17 18:26:06 +0530

    modal changes

--- src/components/collections/defaultViewModal/defaultViewModal.jsx
+++ src/components/collections/defaultViewModal/defaultViewModal.jsx
@@ -2,46 +2,108 @@ import React, { Component } from 'react'
 import DocIcon from '../../../assets/icons/doc.svg'
 import ApiIcon from '../../../assets/icons/api.svg'
 import InfoIcon from '../../../assets/icons/info.svg'
+import shortid from 'shortid'
+import Joi from 'joi-browser'
 import './defaultViewModal.scss'
-import { Modal } from 'react-bootstrap'
+import { Modal, Button } from 'react-bootstrap'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import { addNewTab } from '../../tabs/redux/tabsActions'
-import { onEnter } from '../../common/utility'
+import { onEnter, toTitleCase } from '../../common/utility'
 import Form from '../../common/form'
-import PageForm from '../../pages/pageForm'
+import { addPage1 } from '../../pages/redux/pagesActions'
 export const defaultViewTypes = {
   TESTING: 'testing',
   DOC: 'doc'
 }
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = (dispatch, ownProps) => {
   return {
-    add_new_tab: () => dispatch(addNewTab())
+    add_new_tab: () => dispatch(addNewTab()),
+    add_page: (rootParentId, newPage) => dispatch(addPage1(ownProps.history, rootParentId, newPage))
   }
 }
 export class DefaultViewModal extends Form {
   constructor(props) {
     super(props)
+    this.inputRef = React.createRef()
     this.state = {
       showPageForm: {
         addPage: false
+      },
+      data: {
+        name: ''
+      },
+      errors: {
+        name: ''
       }
     }
-    this.renderCollectionDetailsForm = this.renderCollectionDetailsForm.bind(this)
+
+    this.schema = {
+      name: Joi.string().required().label('Page name'),
+      contents: Joi.string().allow(null, ''),
+      state: Joi.valid(0, 1, 2, 3)
+    }
+  }
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.doSubmit()
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    // Check if the condition to show the input is now true and was not true before
+    if (this.state.showPageForm.addPage && !prevState.showPageForm.addPage) {
+      // this.inputRef.current && this.inputRef.current.focus();
+      if (this.inputRef.current) {
+        this.inputRef.current.focus()
+      }
+    }
+  }
+
+  async doSubmit() {
+    if (!this.state.selectedCollection && this.props.addEntity) {
+      this.setState({ versionRequired: true })
+      return
+    }
+    const collections = this.props?.selectedCollection
+    this.props.onHide()
+    let { name } = { ...this.state?.data }
+    name = toTitleCase(name)
+    if (this.props.title === 'Add Parent Page' || this.props.addEntity) {
+      const rootParentId = collections?.rootParentId
+      const data = { ...this.state.data, name }
+      const newPage = {
+        ...data,
+        requestId: shortid.generate(),
+        versionId: this.props.pageType === 1 ? shortid.generate() : null,
+        pageType: this.props.pageType
+      }
+      this.props.add_page(rootParentId, newPage)
+    }
+    if (this.props?.title === 'Add Page' || this.props?.title === 'Add Sub Page' || this.props?.addEntity) {
+      const selectedId = this.props?.title === 'Add Page' ? this.props?.selectedVersion : this.props?.selectedPage
+      const ParentId = selectedId
+      const data = { ...this.state.data }
+      const newPage = {
+        ...data,
+        requestId: shortid.generate(),
+        versionId: this.props?.pageType === 1 ? shortid.generate() : null,
+        pageType: this.props?.pageType,
+        state: 0
+      }
+      this.props.add_page(ParentId, newPage)
+    }
+  }
   renderCollectionDetailsForm() {
     return (
-      this.state.showPageForm.addPage && (
-        <PageForm
-          {...this.props}
-          show={this.state.showPageForm.addPage}
-          onHide={() => this.props.onHide()}
-          title={this.props.title}
-          pageType={this.props.pageType}
-        />
-      )
+      <div className='mt-5'>
+        {/* <div className='d-flex gap-5 justify-content-center mt-5'> */}
+        {this.renderInput('name', this.props.title, 'page name', true, true)}
+        <div className='text-left mt-4 mx-2'>
+          <Button onClick={this.handleSubmit}>Submit</Button>
+        </div>
+        {/* </div> */}
+      </div>
     )
   }
 
@@ -56,7 +118,12 @@ export class DefaultViewModal extends Form {
 
   renderDocButton() {
     return (
-      <button className='block-view-btn' onClick={() => this.setState({ showPageForm: { addPage: true } })}>
+      <button
+        className='block-view-btn'
+        onClick={() => {
+          this.setState({ showPageForm: { addPage: true } })
+        }}
+      >
         <img src={DocIcon} alt='' />
         {'Create Page'}
       </button>
@@ -69,8 +136,8 @@ export class DefaultViewModal extends Form {
         <div className='d-flex justify-content-center'>
           {this.renderTestingButton()}
           {this.renderDocButton()}
-          {this.renderCollectionDetailsForm()}
         </div>
+        {this.state.showPageForm.addPage && this.renderCollectionDetailsForm()}
         <div className='info mt-5 d-flex align-items-center'>
           <img src={InfoIcon} className='mr-2' alt='' />
           <span>You can always choose to Test the API's or make the Testing API description</span>
