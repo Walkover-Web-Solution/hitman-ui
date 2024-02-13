5b6fcac040c32d94da60f41a29a3dedc801cedde
idris bohra <idrisbohra786@gmail.com>
2024-02-13 15:39:21 +0530

    endpoint and pages are now being saved without any bug and commented the usefull code and publish page is now being shown with the default title of publish collection

--- src/components/groups/groups.jsx
+++ src/components/groups/groups.jsx
@@ -440,7 +440,7 @@ class Groups extends Component {
                       </svg>{' '}
                       Duplicate
                     </div> */}
-                      <div className='dropdown-item' onClick={() => this.openShareSubPageForm(subPageId)}>
+                      {/* <div className='dropdown-item' onClick={() => this.openShareSubPageForm(subPageId)}>
                         <svg width='18' height='18' viewBox='0 0 18 18' fill='none' xmlns='http://www.w3.org/2000/svg'>
                           <path
                             d='M13.5 6C14.7426 6 15.75 4.99264 15.75 3.75C15.75 2.50736 14.7426 1.5 13.5 1.5C12.2574 1.5 11.25 2.50736 11.25 3.75C11.25 4.99264 12.2574 6 13.5 6Z'
@@ -479,7 +479,7 @@ class Groups extends Component {
                           />
                         </svg>{' '}
                         Share
-                      </div>
+                      </div> */}
                     </div>
                   </div>
                 ) : null
