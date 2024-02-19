57f6d0e85a1b86e0bbc2e785f24a0be209bc23d6
chirag <chiragagrawal61230@gmail.com>
2024-02-19 18:21:47 +0530

    import all the svg as react component

--- src/components/collectionVersions/collectionVersions.jsx
+++ src/components/collectionVersions/collectionVersions.jsx
@@ -15,9 +15,11 @@ import ExpandArrow from '../../assets/icons/expand-arrow.svg'
 import CombinedCollections from '../combinedCollections/combinedCollections'
 import { addIsExpandedAction } from '../../store/clientData/clientDataActions'
 import DefaultViewModal from '../collections/defaultViewModal/defaultViewModal'
-import editsign from '../../assets/icons/editsign.svg'
-import deleteicon from '../../assets/icons/delete-icon.svg'
-import sharesign from '../../assets/icons/sharesign.svg'
+import {ReactComponent as EditIcon} from '../../assets/icons/editsign.svg'
+import {ReactComponent as DeleteIcon} from '../../assets/icons/delete-icon.svg'
+import {ReactComponent as ShareIcon} from '../../assets/icons/sharesign.svg'
+// import {ReactComponent as Duplicate} from '../../assets/icons/duplicateSign.svg'
+
 
 const mapStateToProps = (state) => {
   return {
@@ -298,7 +300,7 @@ class CollectionVersions extends Component {
                 </div>
                 <div className='dropdown-menu dropdown-menu-right'>
                   <div className='dropdown-item' onClick={() => this.openEditVersionForm(versionId)}>
-                    <img src= {editsign} />{' '}
+                    <EditIcon/> {' '}
                     Edit
                   </div>
                   <div
@@ -307,7 +309,7 @@ class CollectionVersions extends Component {
                       this.openDeleteVersionModal(versionId)
                     }}
                   >
-                   <img src= {deleteicon} />{' '}
+                   <DeleteIcon/> {' '}
                     Delete
                   </div>
                   {/* <div
@@ -316,26 +318,11 @@ class CollectionVersions extends Component {
                       this.handleDuplicate(this.props.versions[versionId])
                     }}
                   >
-                    <svg width='18' height='18' viewBox='0 0 18 18' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                      <path
-                        d='M15 6.75H8.25C7.42157 6.75 6.75 7.42157 6.75 8.25V15C6.75 15.8284 7.42157 16.5 8.25 16.5H15C15.8284 16.5 16.5 15.8284 16.5 15V8.25C16.5 7.42157 15.8284 6.75 15 6.75Z'
-                        stroke='#E98A36'
-                        strokeWidth='1.5'
-                        strokeLinecap='round'
-                        strokeLinejoin='round'
-                      />
-                      <path
-                        d='M3.75 11.25H3C2.60218 11.25 2.22064 11.092 1.93934 10.8107C1.65804 10.5294 1.5 10.1478 1.5 9.75V3C1.5 2.60218 1.65804 2.22064 1.93934 1.93934C2.22064 1.65804 2.60218 1.5 3 1.5H9.75C10.1478 1.5 10.5294 1.65804 10.8107 1.93934C11.092 2.22064 11.25 2.60218 11.25 3V3.75'
-                        stroke='#E98A36'
-                        strokeWidth='1.5'
-                        strokeLinecap='round'
-                        strokeLinejoin='round'
-                      />
-                    </svg>{' '}
+                    <Duplicate/> {' '}
                     Duplicate
                   </div> */}
                   <div className='dropdown-item' onClick={() => this.openShareVersionForm(this.props.versions[versionId])}>
-                    <img src= {sharesign} />
+                    <ShareIcon/>
                     Share
                   </div>
                 </div>
