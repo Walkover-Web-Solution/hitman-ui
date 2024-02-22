32ff89afd0ec911d74f9a3883323396806e68d17
idris bohra <idrisbohra786@gmail.com>
2024-02-22 16:20:53 +0530

    version ui done

---
+++ src/components/collectionVersions/versionPublishDropdown/versionPublishDropdown.jsx
@@ -0,0 +1,25 @@
+// import React from 'react'
+// import { DropdownButton } from 'react-bootstrap'
+
+// export default function VersionPublishDropdown(props) {
+//   return (
+//     <DropdownButton
+//       className=''
+//       id='dropdown-basic-button'
+//       onClick={(event) => event.stopPropagation()}
+//       title={
+//         <span className='dropdown-title'>
+//           {this.props.pages?.[this.props.rootParentId]?.child?.length === 1
+//             ? this.state.defaultVersionName
+//             : this.state.selectedVersionName}
+//         </span>
+//       }
+//     >
+//       {this.props.pages[rootId].child.map((childId, index) => (
+//         <Dropdown.Item key={index} onClick={(e) => this.handleDropdownItemClick(childId, rootId)}>
+//           <span className='dropdown-item-text'>{this.props.pages[childId]?.name}</span>
+//         </Dropdown.Item>
+//       ))}
+//     </DropdownButton>
+//   )
+// }
