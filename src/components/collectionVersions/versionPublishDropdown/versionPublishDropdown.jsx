06b18de68623eaa3cfe1ec77c998c78743b0d16f
Rashiik <rashikhandelwal269@gmail.com>
2024-02-23 15:06:42 +0530

    dropdown version

--- src/components/collectionVersions/versionPublishDropdown/versionPublishDropdown.jsx
+++ src/components/collectionVersions/versionPublishDropdown/versionPublishDropdown.jsx
@@ -1,25 +1,48 @@
-// import React from 'react'
-// import { DropdownButton } from 'react-bootstrap'
+import React, { useEffect, useState } from 'react'
+import { Dropdown, DropdownButton } from 'react-bootstrap'
+import { useSelector } from 'react-redux'
 
-// export default function VersionPublishDropdown(props) {
-//   return (
-//     <DropdownButton
-//       className=''
-//       id='dropdown-basic-button'
-//       onClick={(event) => event.stopPropagation()}
-//       title={
-//         <span className='dropdown-title'>
-//           {this.props.pages?.[this.props.rootParentId]?.child?.length === 1
-//             ? this.state.defaultVersionName
-//             : this.state.selectedVersionName}
-//         </span>
-//       }
-//     >
-//       {this.props.pages[rootId].child.map((childId, index) => (
-//         <Dropdown.Item key={index} onClick={(e) => this.handleDropdownItemClick(childId, rootId)}>
-//           <span className='dropdown-item-text'>{this.props.pages[childId]?.name}</span>
-//         </Dropdown.Item>
-//       ))}
-//     </DropdownButton>
-//   )
-// }
+export default function VersionPublishDropdown(props) {
+    const [defaultVersion, setDefaultVersion] = useState('')
+    const [selectedVersion, setSelectedVersion] = useState('')
+
+  useEffect(() => {
+    var defaultVersion = findDefaultVersion()
+    setDefaultVersion(defaultVersion)
+  }, [])
+
+  const { pages } = useSelector((state) => {
+    return { pages: state.pages }
+  })
+  const handleDropdownItemClick = (childId) => {
+    var selected = pages[childId].name
+    console.log(selected, "selecteddd");
+    setSelectedVersion(...selected)
+    console.log('inisde handle dropdown clickedddd', childId)
+  }
+
+  const findDefaultVersion = () => {
+    const children = pages[props.rootParentId]?.child || []
+    return children.map((childId) => pages[childId]).find((page) => page?.state === 1)
+  }
+  
+console.log(selectedVersion.name, "selected version nameee");
+  return (
+    <DropdownButton
+      className=''
+      id='dropdown-basic-button'
+      onClick={(event) => event.stopPropagation()}
+      title={
+        <span className='dropdown-title'>
+          {pages?.[props.rootParentId]?.child?.length === 1 ? defaultVersion?.name : selectedVersion?.name}
+        </span>
+      }
+    >
+      {pages[props.rootParentId].child.map((childId, index) => (
+        <Dropdown.Item key={index} onClick={(e) => handleDropdownItemClick(childId)}>
+          <span className='dropdown-item-text'>{pages[childId]?.name}</span>
+        </Dropdown.Item>
+      ))}
+    </DropdownButton>
+  )
+}
