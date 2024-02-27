43f9a2c61fab15fd76e75628c5cbf6b0c799fa3b
Rashiik <rashikhandelwal269@gmail.com>
2024-02-27 15:50:57 +0530

    resolve conflicts

--- src/components/auth/authServiceV2.js
+++ src/components/auth/authServiceV2.js
@@ -128,9 +128,8 @@ function AuthServiceV2() {
           <Modal.Title>Select Organization</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          {/* dropdown removed */}
           <div className="org-listing-container ">
-            <div className="org-listing-column d-flex flex-column">
+            <div className="org-listing-column auth d-flex flex-column">
               {orgList?.map((org, key) => (
                 <button
                   className="btn btn-primary mb-2 p-2"
