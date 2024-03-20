76581b980237202195bd7a826a22389386cc3e5d
idris bohra <idrisbohra786@gmail.com>
2024-03-20 15:48:29 +0530

    updates in collection tab, endpoint handle, cookie trim

--- src/components/common/utility.js
+++ src/components/common/utility.js
@@ -648,7 +648,7 @@ const deleteSidebarData = (pages, tabs, pageId, deletedTabIds, deletedIds) => {
   }
 }
 
-export const deleteAllPagesAndTabsAndReactQueryData = async (pageId) => {
+export const deleteAllPagesAndTabsAndReactQueryData = async (pageId,collectionId) => {
   try {
     const deletedTabIds = new Set() // to keep set of ids deleted
 
@@ -663,7 +663,17 @@ export const deleteAllPagesAndTabsAndReactQueryData = async (pageId) => {
     let { pages, tabs } = store.getState()
     pages = _.cloneDeep(pages)
     tabs = _.cloneDeep(tabs)
+   
+      if (collectionId && tabs.tabs?.[collectionId]) {
+        delete tabs.tabs[collectionId];
+        tabs.tabsOrder = tabs.tabsOrder.filter(tab => tab !== collectionId);
+        if (tabs?.activeTabId == collectionId) {
+          foundActiveTabId = true
+        }
+        
+    }
 
+    
     // update the parent's child
     let parentId = pages[pageId].parentId
     if (parentId != null) {
