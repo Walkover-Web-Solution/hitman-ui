cd6d37525b5dff349aa801255030d630a68b90d3
idris bohra <idrisbohra786@gmail.com>
2024-04-18 13:16:21 +0530

    Have completed the icon work, showing it on the tabs

--- src/components/tabs/tabs.jsx
+++ src/components/tabs/tabs.jsx
@@ -137,17 +137,17 @@ class CustomTabs extends Component {
           const endpoint = this.props.pages[tabId]
           if (tab.previewMode) {
             return (
-              <>
-                {this.props.pages[tabId]?.name}
-                <span className='sub-label'>{this.props.groups[endpoint.groupId]?.name}</span>
-              </>
+              <div>
+                <div className={`${this.props.tabState[tabId]?.draft?.data?.method}-TAB mr-2 request-type-bgcolor`}>{this.props.tabState[tabId]?.draft?.data?.method}</div>
+                <span>{this.props.pages[tabId]?.name}</span>
+              </div>
             )
           } else {
             return (
-              <>
-                {this.props.pages[tabId]?.name}
-                <span className='sub-label'>{this.props.groups[endpoint.groupId]?.name}</span>
-              </>
+              <div>
+                <div className={`${this.props.tabState[tabId]?.draft?.data?.method}-TAB mr-2 request-type-bgcolor`}>{this.props.tabState[tabId]?.draft?.data?.method}</div>
+                <span>{this.props.pages[tabId]?.name}</span>
+              </div>
             )
           }
         } else {
@@ -159,11 +159,10 @@ class CustomTabs extends Component {
           const page = this.props.pages[tabId]
           if (tab.previewMode) {
             return (
-              <>
-                {this.props.pages[tabId].name}
-                {/* <span>div</span> */}
-                {/* <span className='sub-label'>{this.props.groups[page.groupId]?.name || this.props.versions[page.versionId]?.number}</span> */}
-              </>
+              <div className='d-flex align-items-center'>
+                <HiMiniDocumentText className='mr-1' size={16} />
+                <span>{page.name}</span>
+              </div>
             )
           } else {
             return (
@@ -290,9 +289,8 @@ class CustomTabs extends Component {
           <div className='hover-div' style={styles}>
             {/* <div className='group-name'>{this.props.pages[tabId]?.name}</div> */}
             <div className='d-flex align-items-center'>
-              <div className={`api-label ${this.props.pages[tabId]?.requestType} request-type-bgcolor ml-4 mt-1 arrow-top`}>
-                {' '}
-                {this.props.pages[tabId]?.requestType}{' '}
+              <div className={`api-label ${this.props.tabState[tabId]?.draft?.data?.method} request-type-bgcolor ml-4 mt-1 arrow-top`}>
+                {this.props.tabState[tabId]?.draft?.data?.method}{' '}
               </div>
               <div className='endpoint-name ml-1'>{this.props.pages[tabId].name}</div>
             </div>
