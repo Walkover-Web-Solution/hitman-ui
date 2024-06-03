868fa4ede73e8206947a7773605a53e9ea2a645f
Saurabh186 <sawrabhsharma1234@gmail.com>
2024-06-03 20:38:11 +0530

    fix save button

--- src/components/endpoints/displayEndpoint.jsx
+++ src/components/endpoints/displayEndpoint.jsx
@@ -384,7 +384,8 @@ class DisplayEndpoint extends Component {
       sslMode: getCurrentUserSSLMode(),
       showAskAiSlider: false,
       endpointContentState: null,
-      showEndpointFormModal: false
+      showEndpointFormModal: false,
+      titleChange : false
     }
     this.uri = React.createRef()
     this.paramKey = React.createRef()
@@ -2059,6 +2060,9 @@ class DisplayEndpoint extends Component {
       this.setState({ data: obj })
     }
   }
+  getTitleChanged(isChanged) {
+     this.setState({ titleChange: isChanged });
+  }
 
   renderCookiesModal() {
     return (
@@ -2683,8 +2687,9 @@ class DisplayEndpoint extends Component {
                 id='api_save_btn'
                 className={this.state.saveLoader ? 'btn btn-outline orange buttonLoader btn-sm d-flex align-items-center' : 'btn btn-outline orange btn-sm d-flex align-items-center'}
                 type='button'
+                disabled={!this.state.titleChange}
                 onClick={() => this.handleSave()}
-              >
+                >
                 <LiaSaveSolid className='save-icon mr-1' size={16} />
                 <span>Save</span>
               </button>
@@ -2855,6 +2860,7 @@ class DisplayEndpoint extends Component {
                             old_description={this.state.oldDescription}
                             props_from_parent={this.propsFromDescription.bind(this)}
                             alterEndpointName={(name) => this.alterEndpointName(name)}
+                            getTitleChanged={(isTitleChange) => this.getTitleChanged(isTitleChange)}
                           />
                         )}
                         {this.renderSaveButton()}
--- src/components/endpoints/endpointBreadCrumb.jsx
+++ src/components/endpoints/endpointBreadCrumb.jsx
@@ -174,6 +174,7 @@ class EndpointBreadCrumb extends Component {
 
   handleInputChange(e) {
     this.setState({ changesMade: true });
+    this.props.getTitleChanged(this.state.changesMade);
     if (this.props?.isEndpoint) {
       const tempData = this.props?.endpointContent || {}
       tempData.data.name = e.currentTarget.value
