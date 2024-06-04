56ac504385a54ae8a05af9bd650a08a1b1039947
Saurabh186 <sawrabhsharma1234@gmail.com>
2024-06-04 15:08:30 +0530

    cherry-pick--5

--- src/components/endpoints/displayEndpoint.jsx
+++ src/components/endpoints/displayEndpoint.jsx
@@ -2118,9 +2118,6 @@ class DisplayEndpoint extends Component {
       this.setState({ data: obj })
     }
   }
-  getTitleChanged(isChanged) {
-     this.setState({ titleChange: isChanged });
-  }
 
   renderCookiesModal() {
     return (
@@ -2748,9 +2745,10 @@ class DisplayEndpoint extends Component {
                 id='api_save_btn'
                 className={this.state.saveLoader ? 'btn btn-outline orange buttonLoader btn-sm d-flex align-items-center' : 'btn btn-outline orange btn-sm d-flex align-items-center'}
                 type='button'
-                disabled={!this.state.titleChange}
+                disabled={!this.props?.tabs[this.props?.activeTabId]?.isModified}
                 onClick={() => this.handleSave()}
-                >
+                title={!this.props?.tabs[this.props?.activeTabId]?.isModified ? 'No changes in this request' : 'Save request'}
+              >
                 <LiaSaveSolid className='save-icon mr-1' size={16} />
                 <span>Save</span>
               </button>
@@ -2954,7 +2952,6 @@ class DisplayEndpoint extends Component {
                             old_description={this.state.oldDescription}
                             props_from_parent={this.propsFromDescription.bind(this)}
                             alterEndpointName={(name) => this.alterEndpointName(name)}
-                            getTitleChanged={(isTitleChange) => this.getTitleChanged(isTitleChange)}
                           />
                         )}
                         {this.renderSaveButton()}
