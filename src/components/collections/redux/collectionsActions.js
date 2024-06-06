3ed3632a0c6af0bf50c37994abb2fa9b0005d9c9
goutam01mehta <88901454+goutam01mehta@users.noreply.github.com>
2024-06-06 14:54:33 +0530

    handled the condition for move in another org and refactored the code

--- src/components/collections/redux/collectionsActions.js
+++ src/components/collections/redux/collectionsActions.js
@@ -203,13 +203,6 @@ export const onCollectionDeletedError = (error, collection) => {
   }
 }
 
-export const moveCollection = (collection) => {
-  return {
-    type: collectionsActionTypes.MOVE_COLLECTION,
-    payload:collection
-  }
-}
-
 // To do later
 export const duplicateCollection = (collection) => {
   return (dispatch) => {
