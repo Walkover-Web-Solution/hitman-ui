8e27237c7f55c7bfa0784ef48ce070480a3dc716
idris bohra <idrisbohra786@gmail.com>
2024-06-13 00:52:13 +0530

    done with the bug and delete process and validation are still pending

--- src/components/collections/Redirections.jsx
+++ src/components/collections/Redirections.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { ReactComponent as DeleteIcon } from '../../assets/icons/delete-icon.svg'
 import { useSelector } from 'react-redux'
 import { useParams } from 'react-router'
@@ -8,6 +8,7 @@ import './redirections.scss'
 
 const Redirections = () => {
   const params = useParams()
+  const userPathRef = useRef(null)
 
   const { pages, collections, childIds } = useSelector((state) => {
     return {
@@ -17,21 +18,22 @@ const Redirections = () => {
     }
   })
 
-  const [userPath, setUserPath] = useState([])
   const [redirectUrls, setRedirectUrls] = useState([])
   const [selectedPageId, setselectedPageId] = useState(null)
   const [latestUrl, setLatestUrl] = useState('')
 
-  const visiblePath = `${process.env.REACT_APP_UI_URL}/p`
+  const visiblePath = `${process.env.REACT_APP_UI_URL}/p/`
 
   useEffect(() => {
     loadUrls()
   }, [params?.collectionId])
 
   const addOldUrlsOfChilds = (pagesHaveOldUrls, pageId) => {
-    Object.entries(pages[pageId]?.oldUrls).forEach(([index, oldUrl]) => {
-      pagesHaveOldUrls.push({ pageId, oldUrl, oldUrlId: index })
-    })
+    {
+      pages[pageId]?.oldUrls && Object.keys(pages[pageId]?.oldUrls).length > 0 && Object.entries(pages[pageId]?.oldUrls).forEach(([index, oldUrl]) => {
+        pagesHaveOldUrls.push({ pageId, oldUrl, oldUrlId: index })
+      })
+    }
     if (pages?.[pageId]?.child.length > 0) {
       pages[pageId].child.forEach((pageId) => addOldUrlsOfChilds(pagesHaveOldUrls, pageId))
     }
@@ -47,14 +49,15 @@ const Redirections = () => {
       console.log(pagesHaveOldUrls, 12345678)
       setRedirectUrls(pagesHaveOldUrls)
     } catch (error) {
-      console.error('Failed to fetch URLs:', error)
+      console.error('Failed to fetch URLs:', error.message)
     }
   }
 
   const handleAddUrl = async () => {
     try {
-      const result = (await addUrlWithAdditionalPath(selectedPageId, params.collectionId, userPath)).data
-      const updatedRedirectUrls = [...redirectUrls, { pageId: result.pageId, redirectionUrls: [result.oldUrl] }]
+      const url = getWholeUrl(userPathRef.current.value)
+      const result = (await addUrlWithAdditionalPath(selectedPageId, params.collectionId, url)).data
+      const updatedRedirectUrls = [...redirectUrls, { pageId: result.pageId, oldUrl: url, oldUrlId: result.id }]
       setRedirectUrls(updatedRedirectUrls)
     } catch (error) {
       console.error('Failed to create URL:', error)
@@ -64,15 +67,6 @@ const Redirections = () => {
   const handleDeleteUrl = async (id, url) => {
     try {
       await deleteMappedUrl(id)
-      // const updatedOldUrlArray = redirectUrls.map((item) => {
-      //   if (item.pageId === pageId) {
-      //     const updatedOldUrls = { ...item.oldUrls }
-      //     delete updatedOldUrls[id]
-      //     return { ...item, oldUrls: updatedOldUrls }
-      //   }
-      //   return item
-      // })
-      // setRedirectUrls(updatedOldUrlArray)
     } catch (error) {
       console.error('Failed to delete URL:', error)
     }
@@ -81,7 +75,7 @@ const Redirections = () => {
   const setLatestUrlForPage = (id, setInUseState = true) => {
     if (setInUseState) setselectedPageId(id)
     let pathName = getUrlPathById(id, pages)
-    pathName = `/${pathName}`
+    pathName = `${pathName}`
     pathName = pathName.replace('null', params.collectionId)
     if (setInUseState) setLatestUrl(pathName)
     return pathName
@@ -153,19 +147,11 @@ const Redirections = () => {
     )
   }
 
-  const hasCustomDomain = () => {
-    const domain = collections?.[params.collectionId]?.customDomain
-    const url = 'http://' + domain + '/p/'
-    const ownDomain = 'http://techdoc.walkover.in/p/'
-    if (domain !== null) return url
-    return ownDomain
-  }
-
-  const handleInputChange = (e) => {
-    const newValue = e.target.value
-    const prefix = hasCustomDomain()
-    const suffix = '?collectionId=' + params.collectionId
-    setUserPath(prefix + newValue + suffix)
+  const getWholeUrl = (value) => {
+    const customDomain = collections?.[params.collectionId]?.customDomain
+    const urlPrefix = customDomain ? 'http://' + customDomain + '/p/' : visiblePath;
+    const urlSuffix = '?collectionId=' + params.collectionId
+    return urlPrefix + value + urlSuffix;
   }
 
   const getLatestUrl = (pageId) => {
@@ -214,18 +200,15 @@ const Redirections = () => {
       <div className='saperation'></div>
       <div className='d-flex justify-content-center redirection-table-container mt-4'>
         <div className='main-content'>
-          <h1>
-            {collections?.[params.collectionId]?.name.charAt(0).toUpperCase() + collections?.[params.collectionId]?.name.slice(1)}{' '}
-            Redirections{' '}
-          </h1>
+          <h3>{collections?.[params.collectionId]?.name.charAt(0).toUpperCase() + collections?.[params.collectionId]?.name.slice(1)} Redirections</h3>
           <div className='form'>
             <div className='d-flex flex-column flex-shrink-1 flex-grow-1'>
               <input
+                ref={userPathRef}
                 type='text'
                 class='input-part editable h-100'
                 id='part2'
                 placeholder='Enter your path'
-                onChange={(e) => handleInputChange(e)}
               />
               <span>
                 Domain for the path:- <span className='domain-name'>{visiblePath}</span>
