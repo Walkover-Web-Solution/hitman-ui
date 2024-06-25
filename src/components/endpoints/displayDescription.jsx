7598a3a5bdb7d6536c062c240b0d0ccc626360dc
shivm29 <developers@whozzat.com>
2024-06-25 12:40:35 +0530

    commit 1

--- src/components/endpoints/displayDescription.jsx
+++ src/components/endpoints/displayDescription.jsx
@@ -1,99 +1,85 @@
-import React, { Component } from 'react'
-import { isDashboardRoute } from '../common/utility'
-import { updateEndpoint } from '../pages/redux/pagesActions'
-import { connect } from 'react-redux'
-import './endpointBreadCrumb.scss'
-import EndpointBreadCrumb from './endpointBreadCrumb'
+import React, { useState, useEffect } from 'react';
+import { isDashboardRoute } from '../common/utility';
+import { updateEndpoint } from '../pages/redux/pagesActions';
+import { connect } from 'react-redux';
+import './endpointBreadCrumb.scss';
+import EndpointBreadCrumb from './endpointBreadCrumb';
 
-const mapDispatchToProps = (dispatch, ownProps) => {
+const mapDispatchToProps = (dispatch) => {
   return {
-    update_endpoint: (editedEndpoint) => dispatch(updateEndpoint(editedEndpoint))
-  }
-}
+    update_endpoint: (editedEndpoint) => dispatch(updateEndpoint(editedEndpoint)),
+  };
+};
 
-class DisplayDescription extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      showDescriptionFormFlag: false,
-      showAddDescriptionFlag: isDashboardRoute(this.props)
-        ? !!(this.props.endpoint.description === '' || this.props.endpoint.description == null)
-        : false,
-      theme: ''
+const DisplayDescription = (props) => {
+  const [showDescriptionFormFlag, setShowDescriptionFormFlag] = useState(false);
+  const [showAddDescriptionFlag, setShowAddDescriptionFlag] = useState(
+    isDashboardRoute(props) ? !!(props.endpoint.description === '' || props.endpoint.description == null) : false
+  );
+  const [theme, setTheme] = useState('');
+
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      ['bold', 'italic', 'underline', 'strike'],
+      [{ color: [] }, { background: [] }],
+      [{ list: 'ordered' }, { list: 'bullet' }],
+      ['link'],
+    ],
+  };
+
+  const formats = ['header', 'bold', 'italic', 'underline', 'strike', 'color', 'background', 'list', 'bullet', 'link'];
+
+  const handleChange = (e) => {
+    const data = { ...props.data };
+    data[e.currentTarget.name] = e.currentTarget.value;
+    props.props_from_parent('data', data);
+  };
+
+  useEffect(() => {
+    if (!theme) {
+      setTheme(props.publicCollectionTheme);
     }
+  }, [theme, props.publicCollectionTheme]);
 
-    this.modules = {
-      toolbar: [
-        [{ header: [1, 2, 3, 4, 5, 6, false] }],
-        ['bold', 'italic', 'underline', 'strike'],
-        [{ color: [] }, { background: [] }],
+  const handleDescription = () => {
+    setShowDescriptionFormFlag(true);
+    setShowAddDescriptionFlag(false);
+  };
 
-        [({ list: 'ordered' }, { list: 'bullet' })],
-        ['link']
-      ]
-    }
+  const handleDescriptionCancel = () => {
+    const endpoint = { ...props.endpoint };
+    endpoint.description = props.old_description;
+    setShowDescriptionFormFlag(false);
+    setShowAddDescriptionFlag(true);
+    props.props_from_parent('endpoint', endpoint);
+  };
 
-    this.formats = ['header', 'bold', 'italic', 'underline', 'strike', 'color', 'background', 'list', 'bullet', 'link']
-  }
+  const handleDescriptionSave = (e) => {
+    e.preventDefault();
+    const value = props.endpoint.description;
+    const endpoint = { ...props.endpoint };
+    props.update_endpoint({ id: endpoint.id, description: value });
+    endpoint.description = value;
+    setShowDescriptionFormFlag(false);
+    setShowAddDescriptionFlag(true);
+    props.props_from_parent('endpoint', endpoint);
+    props.props_from_parent('oldDescription', value);
+  };
 
-  handleChange(e) {
-    const data = { ...this.props.data }
-    data[e.currentTarget.name] = e.currentTarget.value
-    this.props.props_from_parent('data', data)
-  }
+  const handleChangeDescription = (value) => {
+    const endpoint = { ...props.endpoint };
+    endpoint.description = value;
+    props.props_from_parent('endpoint', endpoint);
+  };
 
-  componentDidMount() {
-    if (!this.state.theme) {
-      this.setState({ theme: this.props.publicCollectionTheme })
-    }
-  }
-
-  handleDescription() {
-    const showDescriptionFormFlag = true
-    const showAddDescriptionFlag = false
-    this.setState({ showDescriptionFormFlag, showAddDescriptionFlag })
-  }
-
-  handleDescriptionCancel() {
-    const endpoint = { ...this.props.endpoint }
-    endpoint.description = this.props.old_description
-    const showDescriptionFormFlag = false
-    this.setState({
-      showDescriptionFormFlag,
-      showAddDescriptionFlag: true
-    })
-    this.props.props_from_parent('endpoint', endpoint)
-  }
-
-  handleDescriptionSave(e) {
-    e.preventDefault()
-    const value = this.props.endpoint.description
-    const endpoint = { ...this.props.endpoint }
-    this.props.update_endpoint({ id: endpoint.id, description: value })
-    endpoint.description = value
-    this.setState({
-      showDescriptionFormFlag: false,
-      showAddDescriptionFlag: true
-    })
-    this.props.props_from_parent('endpoint', endpoint)
-    this.props.props_from_parent('oldDescription', value)
-  }
-
-  handleChangeDescription = (value) => {
-    const endpoint = { ...this.props.endpoint }
-    endpoint.description = value
-    this.props.props_from_parent('endpoint', endpoint)
-  }
-
-  render() {
-    return (
-      <div className='endpoint-header'>
-        <div className={isDashboardRoute(this.props) ? 'panel-endpoint-name-container' : 'endpoint-name-container'}>
-          {isDashboardRoute(this.props) && <>{this.props.endpoint && <EndpointBreadCrumb setActiveTab={this.props.setActiveTab} {...this.props} isEndpoint />}</>}
-        </div>
+  return (
+    <div className='endpoint-header'>
+      <div className={isDashboardRoute(props) ? 'panel-endpoint-name-container' : 'endpoint-name-container'}>
+        {isDashboardRoute(props) && <>{props.endpoint && <EndpointBreadCrumb {...props} isEndpoint />}</>}
       </div>
-    )
-  }
-}
+    </div>
+  );
+};
 
-export default connect(null, mapDispatchToProps)(DisplayDescription)
+export default connect(null, mapDispatchToProps)(DisplayDescription);
