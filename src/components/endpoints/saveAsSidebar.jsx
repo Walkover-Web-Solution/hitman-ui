076686a03701f7479fe980792131c36cfd474539
shivm29 <developers@whozzat.com>
2024-06-26 11:44:03 +0530

    Done

--- src/components/endpoints/saveAsSidebar.jsx
+++ src/components/endpoints/saveAsSidebar.jsx
@@ -1,156 +1,155 @@
-import Joi from 'joi-browser'
-import React, { createRef } from 'react'
-import { connect } from 'react-redux'
-import Form from '../common/form'
-import './endpoints.scss'
-import ShowCaseSaveAsModal from './showCaseSaveAsModal/showCaseSaveAsModal'
-import Input from '../common/input'
-import { trimString } from '../common/utility'
-import  IconButtons  from '../common/iconButton'
+import React, { useEffect, useRef, useState } from 'react';
+import Joi from 'joi-browser';
+import { connect } from 'react-redux';
+import Form from '../common/form';
+import './endpoints.scss';
+import ShowCaseSaveAsModal from './showCaseSaveAsModal/showCaseSaveAsModal';
+import Input from '../common/input';
+import { trimString } from '../common/utility';
+import IconButtons from '../common/iconButton';
 import { MdOutlineClose } from "react-icons/md";
 
 const mapStateToProps = (state) => {
   return {
     collections: state.collections,
     versions: state.versions,
-    pages: state.pages
-  }
-}
+    pages: state.pages,
+  };
+};
 
-class SaveAsSidebar extends Form {
-  constructor(props) {
-    super(props)
-    this.state = {
-      data: {
-        name: '',
-        description: ''
-      }
-    }
-    this.saveAsSidebar = createRef()
-    this.schema = {
-      name: Joi.string().required().label('Endpoint Name'),
-      description: Joi.string().allow(null, '').label('Description')
-    }
-  }
+const SaveAsSidebar = (props) => {
+  const [data, setData] = useState({
+    name: '',
+    description: '',
+  });
+  const saveAsSidebar = useRef(null);
+  const inputRef = useRef(null);
 
-  componentDidMount() {
-    const data = { ...this.state.data }
-    data.name = this.props?.name
-    this.setState({ data })
-    this.saveAsSidebar.focus()
-  }
+  const schema = {
+    name: Joi.string().required().label('Endpoint Name'),
+    description: Joi.string().allow(null, '').label('Description'),
+  };
 
-  handleEndpointNameChange(e) {
-    const dummyData = this.props?.endpointContent
-    dummyData.data.name = e.currentTarget.value
-    this.setState({ data: { ...this.state.data, name: e.currentTarget.value } })
-    this.props.setQueryUpdatedData(dummyData)
-  }
+  useEffect(() => {
+    const newData = { ...data, name: props?.name };
+    setData(newData);
+  }, [props?.name]);
 
-  handleEndpointNameBlur(e) {
+  const handleEndpointNameChange = (e) => {
+    const dummyData = { ...props?.endpointContent };
+    dummyData.data.name = e.currentTarget.value;
+    props.setQueryUpdatedData(dummyData);
+  };
+
+  const handleEndpointNameBlur = (e) => {
     if (!trimString(e.currentTarget.value)) {
-      if (this.props?.match?.params?.endpointId !== 'new') {
-        this.props.setQueryUpdatedData({
-          ...this.props.endpointContent,
-          data: { ...this.props.endpointContent.data, name: this.props.pages?.[this.props?.match?.params?.endpointId]?.name || '' }
-        })
+      if (props?.match?.params?.endpointId !== 'new') {
+        props.setQueryUpdatedData({
+          ...props.endpointContent,
+          data: {
+            ...props.endpointContent.data,
+            name: props.pages?.[props?.match?.params?.endpointId]?.name || '',
+          },
+        });
       } else {
-        this.props.setQueryUpdatedData({ ...this.props.endpointContent, data: { ...this.props.endpointContent.data, name: 'Untitled' } })
+        props.setQueryUpdatedData({
+          ...props.endpointContent,
+          data: { ...props.endpointContent.data, name: 'Untitled' },
+        });
       }
     }
-  }
+  };
 
-  handleEndpointSaveAsEndpointNameChange(e) {
-    this.setState({ data: { ...this.state.data, name: e.currentTarget.value } })
-  }
+  const handleEndpointSaveAsEndpointNameChange = (e) => {
+    setData({ ...data, name: e.currentTarget.value });
+  };
 
-  handleSaveAsEndpointNameBlur(e) {
+  const handleSaveAsEndpointNameBlur = (e) => {
     if (!trimString(e.currentTarget.value)) {
-      this.setState({ data: { ...this.state.data, name: this.props?.endpointContent?.data?.name } })
+      setData({ ...data, name: props?.endpointContent?.data?.name });
     }
-  }
+  };
 
-  renderEndpointNameInput() {
+  const renderEndpointNameInput = () => {
     return (
       <Input
-        ref={this.endpointInputRef}
-        value={this.props?.endpointContent?.data?.name || ''}
-        onChange={(e) => this.handleEndpointNameChange(e)}
-        onBlur={(e) => this.handleEndpointNameBlur(e)}
-        // error={errors?.[name]}
+        ref={inputRef}
+        value={props?.endpointContent?.data?.name || ''}
+        onChange={handleEndpointNameChange}
+        onBlur={handleEndpointNameBlur}
         placeholder={'Endpoint Name'}
         mandatory={'mandatory'}
         firstLetterCapitalize
         label={'Name'}
       />
-    )
-  }
+    );
+  };
 
-  renderSaveAsExistingEndpointInput() {
+  const renderSaveAsExistingEndpointInput = () => {
     return (
       <Input
-        value={this.state.data.name}
-        onChange={(e) => this.handleEndpointSaveAsEndpointNameChange(e)}
-        onBlur={(e) => this.handleSaveAsEndpointNameBlur(e)}
-        // error={errors?.[name]}
+        value={data.name}
+        onChange={handleEndpointSaveAsEndpointNameChange}
+        onBlur={handleSaveAsEndpointNameBlur}
         placeholder={'Endpoint Name'}
         mandatory={'mandatory'}
         firstLetterCapitalize
         label={'Name'}
       />
-    )
-  }
+    );
+  };
 
-  render() {
-    const title = this.state.data.name
-    const saveAsSidebarStyle = {
-      position: 'fixed',
-      background: 'white',
-      zIndex: '1050 ',
-      top: '0px',
-      right: '0px',
-      height: '100vh',
-      width: '35vw',
-      boxShadow: '-25px 25px 43px rgba(0, 0, 0, 0.07)',
-      overflow: 'hidden'
-    }
+  const title = data.name;
+  const saveAsSidebarStyle = {
+    position: 'fixed',
+    background: 'white',
+    zIndex: '1050',
+    top: '0px',
+    right: '0px',
+    height: '100vh',
+    width: '35vw',
+    boxShadow: '-25px 25px 43px rgba(0, 0, 0, 0.07)',
+    overflow: 'hidden',
+  };
 
-    return (
-      <>
-        <div
-          tabIndex={-1}
-          ref={(e) => {
-            this.saveAsSidebar = e
-          }}
-          style={saveAsSidebarStyle}
-          className='save-as-sidebar-container'
-        >
-          <div className='custom-collection-modal-container modal-header align-items-center'>
-            <div className='modal-title h4'>{this.props.location.pathname.split('/')[5] !== 'new' ? 'Save As' : 'Save'}</div>
-            <IconButtons><MdOutlineClose className='fs-2'  onClick={() => {
-              this.props.onHide()
-            }}/></IconButtons>
+  return (
+    <div
+      tabIndex={-1}
+      ref={saveAsSidebar}
+      style={saveAsSidebarStyle}
+      className='save-as-sidebar-container'
+    >
+      <div className='custom-collection-modal-container modal-header align-items-center'>
+        <div className='modal-title h4'>{props.location.pathname.split('/')[5] !== 'new' ? 'Save As' : 'Save'}</div>
+        <IconButtons>
+          <MdOutlineClose
+            className='fs-2'
+            onClick={props.onHide}
+          />
+        </IconButtons>
+      </div>
+      <div className='drawer-body'>
+        <form className='desc-box form-parent' onSubmit={props.handleSubmit}>
+          <div className='p-form-group mb-3'>
+            {props?.match?.params?.endpointId === 'new'
+              ? renderEndpointNameInput()
+              : renderSaveAsExistingEndpointInput()}
+            {title?.trim() === '' || title === 'Untitled' ? (
+              <small className='text-danger'>Please enter the Title</small>
+            ) : (
+              <div />
+            )}
           </div>
-          <div className='drawer-body'>
-            <form className='desc-box form-parent' onSubmit={this.handleSubmit}>
-              <div className='p-form-group mb-3'>
-                {this.props?.match?.params?.endpointId === 'new'
-                  ? this.renderEndpointNameInput()
-                  : this.renderSaveAsExistingEndpointInput()}
-                {title?.trim() === '' || title === 'Untitled' ? <small className='text-danger'>Please enter the Title</small> : <div />}
-              </div>
-            </form>
-            <ShowCaseSaveAsModal
-              save_endpoint={this.props.save_endpoint}
-              name={this.state.data.name}
-              description={this.state.data.description}
-              onHide={this.props.onHide}
-            />
-          </div>
-        </div>
-      </>
-    )
-  }
-}
+        </form>
+        <ShowCaseSaveAsModal
+          save_endpoint={props.save_endpoint}
+          name={data.name}
+          description={data.description}
+          onHide={props.onHide}
+        />
+      </div>
+    </div>
+  );
+};
 
-export default connect(mapStateToProps, null)(SaveAsSidebar)
+export default connect(mapStateToProps, null)(SaveAsSidebar);
