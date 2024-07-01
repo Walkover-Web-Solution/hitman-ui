1e2379f4cb3e1a72db860f9590f5155fb776f63a
goutam01mehta <88901454+goutam01mehta@users.noreply.github.com>
2024-07-01 18:17:02 +0530

    integrated the chatbot

--- src/components/endpoints/script/script.jsx
+++ src/components/endpoints/script/script.jsx
@@ -5,6 +5,8 @@ import 'ace-builds/src-noconflict/theme-github'
 import 'ace-builds/webpack-resolver'
 import AceEditor from 'react-ace'
 import { Snippets, preReqSnippets, postReqSnippets } from './snippets'
+import { BiSolidBot } from "react-icons/bi";
+import { getCurrentUser, getProxyToken } from '../../auth/authServiceV2'
 
 export class Script extends Component {
   constructor(props) {
@@ -28,6 +30,11 @@ export class Script extends Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
+    const userId = getCurrentUser()?.id
+    window.SendDataToChatbot({ bridgeName: 'scripts', threadId: `${userId}`,parentId: '',
+    fullScreen: 'false',
+    hideCloseButton: 'false',
+    hideIcon: 'false', variables: {Proxy_auth_token : getProxyToken(), endpoint: this.props.endpointContent}});
     if (this.props.scriptText && !this.scriptFetched && this.props.scriptText !== prevProps.scriptText) {
       this.setState({ scriptEditorText: this.props.scriptText || '' })
       this.scriptFetched = true
@@ -70,11 +77,11 @@ export class Script extends Component {
     editor.focus();
   };
 
-
   renderScriptEditor() {
     return (
-      <div className='col-8'>
+      <div className='col-8 position-relative'>
         {' '}
+        <button className='ai-button-editor position-absolute ' onClick={() => window.openChatbot()}><BiSolidBot /></button>
         <AceEditor
           className='custom-raw-editor'
           mode='javascript'
