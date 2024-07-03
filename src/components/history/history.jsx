fca4713c443e5ef956bdf43e28203fa957a18780
Riya Bansal <riyabansalofficial2814@gmail.com>
2024-07-03 16:19:06 +0530

    history module changed

--- src/components/history/history.jsx
+++ src/components/history/history.jsx
@@ -1,109 +1,74 @@
-import React, { Component } from 'react'
-import moment from 'moment'
-import { connect } from 'react-redux'
-import { ReactComponent as EmptyHistory } from '../../assets/icons/emptyHistroy.svg'
-import { Dropdown } from 'react-bootstrap'
-import { GrGraphQl } from 'react-icons/gr'
-import './history.scss'
+import React, { useState, useEffect } from 'react';
+import moment from 'moment';
+import { useSelector } from 'react-redux';
+import { useHistory, useParams } from 'react-router-dom';
+import { ReactComponent as EmptyHistory } from '../../assets/icons/emptyHistroy.svg';
+import { Dropdown } from 'react-bootstrap';
+import { GrGraphQl } from 'react-icons/gr';
+import './history.scss';
 
 function compareByCreatedAt(a, b) {
-  const t1 = a?.createdAt
-  const t2 = b?.createdAt
-  let comparison = 0
+  const t1 = a?.createdAt;
+  const t2 = b?.createdAt;
+  let comparison = 0;
   if (t1 < t2) {
-    comparison = 1
+    comparison = 1;
   } else if (t1 > t2) {
-    comparison = -1
+    comparison = -1;
   }
-  return comparison
+  return comparison;
 }
 
-const mapStateToProps = (state) => {
-  return {
-    endpoints: state.pages,
-    collections: state.collections
-  }
-}
+const History = () => {
+  const history = useHistory();
+  const { orgId } = useParams();
 
-class History extends Component {
-  state = {
-    show: false,
-    historySnapshot: null
-  }
+  const endpoints = useSelector((state) => state.pages);
+  const collections = useSelector((state) => state.collections);
+  const historySnapshots = useSelector((state) => state.history);
 
-  componentDidMount() {
-    if (this.props.historySnapshots) {
-      this.setState({
-        historySnapshot: Object.values(this.props.historySnapshots)
-      })
+  const [historySnapshot, setHistorySnapshot] = useState([]);
+
+  useEffect(() => {
+    if (historySnapshots) {
+      setHistorySnapshot(Object.values(historySnapshots));
     }
-  }
+  }, [historySnapshots]);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.props.historySnapshots !== prevProps.historySnapshots) {
-      this.setState({
-        historySnapshot: Object.values(this.props.historySnapshots)
-      })
-    }
-  }
-
-  openHistorySnapshot(id) {
-    this.props.history.push({
-      pathname: `/orgs/${this.props.match.params.orgId}/dashboard/history/${id}`,
+  const openHistorySnapshot = (id) => {
+    history.push({
+      pathname: `/orgs/${orgId}/dashboard/history/${id}`,
       historySnapshotId: id
-    })
-  }
+    });
+  };
 
-  renderName(history) {
-    const baseUrl = history?.endpoint?.BASE_URL ? history?.endpoint?.BASE_URL + history?.endpoint?.uri : history?.endpoint?.uri
-    const endpointName = history?.endpoint?.name || baseUrl || 'Random Trigger'
-    return endpointName
-  }
+  const renderName = (history) => {
+    const baseUrl = history?.endpoint?.BASE_URL ? history?.endpoint?.BASE_URL + history?.endpoint?.uri : history?.endpoint?.uri;
+    const endpointName = history?.endpoint?.name || baseUrl || 'Random Trigger';
+    return endpointName;
+  };
 
-  // renderPath(id) {
-  //   let path = ''
-  //   let groupId = null
-  //   let versionId = null
-  //   let collectionId = null
-  //   let endpointId = null
-
-  //   endpointId = id
-  //   groupId = this.props.endpoints[endpointId]?.groupId
-  //   versionId = this.props.groups[groupId]?.versionId
-  //   collectionId = this.props.versions[versionId]?.collectionId
-  //   path =
-  //     this.props.collections[collectionId]?.name + ' > ' + this.props.versions[versionId]?.number + ' > ' + this.props.groups[groupId]?.name
-
-  //   if (id && path) {
-  //     return (
-  //       <div style={{ fontSize: '11px' }} className='text-muted'>
-  //         {path}
-  //       </div>
-  //     )
-  //   } else return <p />
-  // }
-
-  renderHistoryItem(history) {
+  const renderHistoryItem = (history) => {
     return (
       Object.keys(history).length !== 0 && (
         <Dropdown.Item
           key={history.id}
           className='history-option btn d-flex align-items-center mb-2 pt-2'
           onClick={() => {
-            this.openHistorySnapshot(history.id)
+            openHistorySnapshot(history.id);
           }}
         >
-          {history?.endpoint?.protocolType === 1 && <div className={`api-label lg-label ${history?.endpoint?.requestType}`}>
-            <div className='endpoint-request-div'>{history?.endpoint?.requestType}</div>
-          </div>}
-          {history?.endpoint?.protocolType === 2 && <GrGraphQl className='ml-2 graphql-icon' size={14} />
-          }
+          {history?.endpoint?.protocolType === 1 && (
+            <div className={`api-label lg-label ${history?.endpoint?.requestType}`}>
+              <div className='endpoint-request-div'>{history?.endpoint?.requestType}</div>
+            </div>
+          )}
+          {history?.endpoint?.protocolType === 2 && <GrGraphQl className='ml-2 graphql-icon' size={14} />}
           <div className='ml-3'>
             <div className='sideBarListWrapper'>
               <div className='text-left'>
                 <p>
-                  {this.renderName(history)}
-                  {/* {this.renderPath(history.endpoint.id)} */}
+                  {renderName(history)}
                 </p>
               </div>
               <small className='text-muted'>{moment(history.createdAt).format('ddd, Do MMM h:mm a')}</small>
@@ -111,11 +76,10 @@ class History extends Component {
           </div>
         </Dropdown.Item>
       )
-    )
-  }
+    );
+  };
 
-  renderHistoryList() {
-    const { historySnapshot } = this.state
+  const renderHistoryList = () => {
     if (!historySnapshot || historySnapshot.length === 0) {
       return (
         <div className='empty-collections text-center'>
@@ -126,36 +90,38 @@ class History extends Component {
             <h5>No History available.</h5>
           </div>
         </div>
-      )
+      );
     }
-    const groupedHistory = {}
+
+    const groupedHistory = {};
 
     historySnapshot.forEach((history) => {
-      const today = moment().startOf('day')
-      const createdAtMoment = moment(history.createdAt)
-      let dateGroup
+      const today = moment().startOf('day');
+      const createdAtMoment = moment(history.createdAt);
+      let dateGroup;
 
       if (today.isSame(createdAtMoment, 'day')) {
-        dateGroup = 'Today'
+        dateGroup = 'Today';
       } else if (createdAtMoment.isSame(today.clone().subtract(1, 'days'), 'day')) {
-        dateGroup = 'Yesterday'
+        dateGroup = 'Yesterday';
       } else {
-        dateGroup = createdAtMoment.format('MMMM D, YYYY')
+        dateGroup = createdAtMoment.format('MMMM D, YYYY');
       }
 
       if (!groupedHistory[dateGroup]) {
-        groupedHistory[dateGroup] = []
+        groupedHistory[dateGroup] = [];
       }
 
-      groupedHistory[dateGroup].push(history)
-    })
+      groupedHistory[dateGroup].push(history);
+    });
+
     const sortedGroupedHistory = Object.entries(groupedHistory).sort(([dateGroupA], [dateGroupB]) => {
-      if (dateGroupA === 'Today') return -1
-      if (dateGroupB === 'Today') return 1
-      if (dateGroupA === 'Yesterday') return -1
-      if (dateGroupB === 'Yesterday') return 1
-      return moment(dateGroupB, ['MMMM D, YYYY']).diff(moment(dateGroupA, ['MMMM D, YYYY']))
-    })
+      if (dateGroupA === 'Today') return -1;
+      if (dateGroupB === 'Today') return 1;
+      if (dateGroupA === 'Yesterday') return -1;
+      if (dateGroupB === 'Yesterday') return 1;
+      return moment(dateGroupB, ['MMMM D, YYYY']).diff(moment(dateGroupA, ['MMMM D, YYYY']));
+    });
 
     const dropdowns = sortedGroupedHistory.map(([dateGroup, histories]) => (
       <ul key={dateGroup}>
@@ -166,21 +132,21 @@ class History extends Component {
               <li
                 key={history.id}
                 onClick={() => {
-                  this.openHistorySnapshot(history.id)
+                  openHistorySnapshot(history.id);
                 }}
               >
-                {this.renderHistoryItem(history)}
+                {renderHistoryItem(history)}
               </li>
             ))}
           </ul>
         </li>
       </ul>
-    ))
-    return <div className='mt-3 dropdown-menu-center px-2'>{dropdowns}</div>
-  }
+    ));
 
-  render() {
-    return this.renderHistoryList()
-  }
-}
-export default connect(mapStateToProps, null)(History)
+    return <div className='mt-3 dropdown-menu-center px-2'>{dropdowns}</div>;
+  };
+
+  return renderHistoryList();
+};
+
+export default History;
