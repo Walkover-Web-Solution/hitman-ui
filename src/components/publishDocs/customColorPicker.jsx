acd2c89ba911247724a7825e43751a38e258eee5
Riya Bansal <riyabansalofficial2814@gmail.com>
2024-07-04 11:37:15 +0530

    publish docs refactored

--- src/components/publishDocs/customColorPicker.jsx
+++ src/components/publishDocs/customColorPicker.jsx
@@ -1,73 +1,49 @@
-import React from 'react'
-import { CustomPicker, TwitterPicker } from 'react-color'
+import React, { useState, useEffect, useCallback } from 'react';
+import { CustomPicker, TwitterPicker } from 'react-color';
 
-const colors = ['#f2994a', '#7DCCEE', '#27AE60', '#F0BD3B', '#DD755E', '#333333']
+const colors = ['#f2994a', '#7DCCEE', '#27AE60', '#F0BD3B', '#DD755E', '#333333'];
 
-class CustomColorPicker extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      data: {
-        theme: ''
-      }
+const CustomColorPicker = (props) => {
+  const [theme, setTheme] = useState(props.theme || '');
+  const [openColorPicker, setOpenColorPicker] = useState(false);
+
+  useEffect(() => {
+    if (props.theme) {
+      setTheme(props.theme);
     }
-  }
+  }, [props.theme]);
 
-  componentDidMount() {
-    if (this.props.theme) {
-      const data = { ...this.state.data }
-      data.theme = this.props.theme
-      this.setState({ data })
-    }
-  }
+  const handleChangeComplete = useCallback(
+    (color) => {
+      setTheme(color.hex);
+      props.set_theme(color.hex);
+    },
+    [props]
+  );
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps !== this.props) {
-      const data = { ...this.state.data }
-      data.theme = this.props.theme
-      this.setState({ data })
-    }
-  }
+  const customColor = {
+    backgroundColor: theme,
+    height: '40px',
+    width: '40px',
+    borderRadius: '4px',
+  };
 
-  toggleColorOption() {
-    this.setState({
-      openColorPicker: !this.state.openColorPicker
-    })
-  }
-
-  handleChangeComplete = (color) => {
-    const data = { ...this.state.data }
-    data.theme = color.hex
-    this.setState({
-      data
-    })
-    this.props.set_theme(color.hex)
-  }
-
-  render() {
-    const customColor = {
-      backgroundColor: this.state.data.theme,
-      height: '40px',
-      width: '40px',
-      borderRadius: '4px',
-    }
-    return (
-      <>
-        <div className='d-flex align-items-center justify-content-between'>
-          <div>
-            <TwitterPicker
-              triangle='hide'
-              colors={colors}
-              color={this.state.data.theme}
-              onChangeComplete={this.handleChangeComplete}
-              width='400px'
-            />
-          </div>
-          <div style={customColor} />
+  return (
+    <>
+      <div className='d-flex align-items-center justify-content-between'>
+        <div>
+          <TwitterPicker
+            triangle='hide'
+            colors={colors}
+            color={theme}
+            onChangeComplete={handleChangeComplete}
+            width='400px'
+          />
         </div>
-      </>
-    )
-  }
-}
+        <div style={customColor} />
+      </div>
+    </>
+  );
+};
 
-export default CustomPicker(CustomColorPicker)
+export default CustomPicker(CustomColorPicker);
--- src/components/publishDocs/publishDocsConfirmModal.jsx
+++ src/components/publishDocs/publishDocsConfirmModal.jsx
@@ -1,52 +1,52 @@
-import React, { Component } from 'react'
-import { Modal } from 'react-bootstrap'
-import './publishDocsConfirmModal.scss'
+import React from 'react';
+import { Modal } from 'react-bootstrap';
+import './publishDocsConfirmModal.scss';
+import { useHistory, useRouteMatch } from 'react-router-dom';
 
-class PublishDocsConfirmModal extends Component {
-  state = {}
+const PublishDocsConfirmModal = (props) => {
+  const history = useHistory();
+  const match = useRouteMatch();
 
-  handleOkay(collectionId) {
+  const handleOkay = (collectionId) => {
     if (collectionId) {
-      this.props.history.push({
-        pathname: `/orgs/${this.props.match.params.orgId}/admin/publish`,
-        search: `?collectionId=${collectionId}`
-      })
+      history.push({
+        pathname: `/orgs/${match.params.orgId}/admin/publish`,
+        search: `?collectionId=${collectionId}`,
+      });
     }
-    this.props.onHide()
-  }
+    props.onHide();
+  };
 
-  render() {
-    return (
-      <Modal
-        size='lg'
-        // dialogClassName={"publish-doc-confirm-modal"}
-        centered
-        onHide={this.props.onHide}
-        show={this.props.show}
-      >
-        <div>
-          <Modal.Header className='custom-collection-modal-container' closeButton>
-            <Modal.Title id='contained-modal-title-vcenter'>All set to publish your API doc, just a few more steps to go</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <ul>
-              <li>Publish the endpoints you wish to make public.</li>
-              <li>{' Publish the collection, once you are finished with the above step.'}</li>
-            </ul>
+  return (
+    <Modal
+      size='lg'
+      // dialogClassName={"publish-doc-confirm-modal"}
+      centered
+      onHide={props.onHide}
+      show={props.show}
+    >
+      <div>
+        <Modal.Header className='custom-collection-modal-container' closeButton>
+          <Modal.Title id='contained-modal-title-vcenter'>
+            All set to publish your API doc, just a few more steps to go
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <ul>
+            <li>Publish the endpoints you wish to make public.</li>
+            <li>{' Publish the collection, once you are finished with the above step.'}</li>
+          </ul>
 
-            <button
-              className='btn btn-primary'
-              onClick={(e) => {
-                this.handleOkay(this.props.collection_id)
-              }}
-            >
-              Okay
-            </button>
-          </Modal.Body>
-        </div>
-      </Modal>
-    )
-  }
-}
+          <button
+            className='btn btn-primary'
+            onClick={() => handleOkay(props.collection_id)}
+          >
+            Okay
+          </button>
+        </Modal.Body>
+      </div>
+    </Modal>
+  );
+};
 
-export default PublishDocsConfirmModal
+export default PublishDocsConfirmModal;
