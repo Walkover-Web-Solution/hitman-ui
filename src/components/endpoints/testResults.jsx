46b3754e296281525eea52a94e32d61b5095acfb
idris bohra <91829557+idris-bohra@users.noreply.github.com>
2024-07-06 12:52:01 +0530

    Revert "refactored : codeTemplate, endpoints, genericTable, hostContainer, pu…"

--- src/components/endpoints/testResults.jsx
+++ src/components/endpoints/testResults.jsx
@@ -1,37 +1,37 @@
-import React from 'react';
-import './testResults.scss';
+import React, { Component } from 'react'
+import './testResults.scss'
 
-const TestResults = ({ tests }) => {
+class TestResults extends Component {
+  state = {}
+  render() {
+    return this.props.tests.length > 0 ? (
+      <div className='test-results-container px-2'>{this.props.tests.map((test, index) => this.renderTestResult(test, index))}</div>
+    ) : (
+      this.renderEmpty()
+    )
+  }
 
-  const renderTestResult = (testcase, index) => {
-    let details = testcase.testName;
+  renderTestResult(testcase, index) {
+    let details = testcase.testName
     if (!testcase.success) {
-      details += ` (${testcase.msg})`;
+      details += ` (${testcase.msg})`
     }
     return (
       <div key={index} className='test-result-item my-2'>
         <div className={`test-badge ${testcase.status}`}>{testcase.status}</div>
         <span className='name'>{details}</span>
       </div>
-    );
-  };
+    )
+  }
 
-  const renderEmpty = () => {
+  renderEmpty() {
     return (
       <div className='px-3 py-5 text-center'>
         <div>There are no tests for this request.</div>
         <small>Write a test script to automate debugging</small>
       </div>
-    );
-  };
+    )
+  }
+}
 
-  return tests.length > 0 ? (
-    <div className='test-results-container px-2'>
-      {tests.map((test, index) => renderTestResult(test, index))}
-    </div>
-  ) : (
-    renderEmpty()
-  );
-};
-
-export default TestResults;
+export default TestResults
