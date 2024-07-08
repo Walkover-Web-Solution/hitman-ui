8e330cef39089b5758192bb424f73e77940c9126
Viasocket Interns <developers@whozzat.com>
2024-07-08 16:21:36 +0530

    Riya/tech 356/refactored publish docs test (#405)
    * publish docs refactored
    
    * save and publish bug solved
    
    * title editable
    
    * logo url placeholder and save publish button
    
    ---------
    
    Co-authored-by: idris bohra <idrisbohra786@gmail.com>
    Co-authored-by: Riya Bansal <riyabansalofficial2814@gmail.com>

--- src/components/publishDocs/customColorPicker.jsx
+++ src/components/publishDocs/customColorPicker.jsx
@@ -1,73 +1,27 @@
-import React from 'react'
-import { CustomPicker, TwitterPicker } from 'react-color'
+import React, { useCallback } from 'react';
+import { CustomPicker, TwitterPicker } from 'react-color';
 
-const colors = ['#f2994a', '#7DCCEE', '#27AE60', '#F0BD3B', '#DD755E', '#333333']
+const colors = ['#f2994a', '#7DCCEE', '#27AE60', '#F0BD3B', '#DD755E', '#333333'];
 
-class CustomColorPicker extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      data: {
-        theme: ''
-      }
-    }
-  }
+const CustomColorPicker = (props) => {
 
-  componentDidMount() {
-    if (this.props.theme) {
-      const data = { ...this.state.data }
-      data.theme = this.props.theme
-      this.setState({ data })
-    }
-  }
+  const handleChangeComplete = useCallback((color) => {
+    props.set_theme(color.hex);
+  }, []);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps !== this.props) {
-      const data = { ...this.state.data }
-      data.theme = this.props.theme
-      this.setState({ data })
-    }
-  }
+  const customColor = {
+    backgroundColor: props.theme,
+    height: '40px',
+    width: '40px',
+    borderRadius: '4px',
+  };
 
-  toggleColorOption() {
-    this.setState({
-      openColorPicker: !this.state.openColorPicker
-    })
-  }
+  return (
+    <div className='d-flex align-items-center justify-content-between'>
+      <TwitterPicker triangle='hide' colors={colors} color={props.theme} onChangeComplete={handleChangeComplete} width='400px' />
+      <div style={customColor} />
+    </div>
+  );
+};
 
-  handleChangeComplete = (color) => {
-    const data = { ...this.state.data }
-    data.theme = color.hex
-    this.setState({
-      data
-    })
-    this.props.set_theme(color.hex)
-  }
-
-  render() {
-    const customColor = {
-      backgroundColor: this.state.data.theme,
-      height: '40px',
-      width: '40px',
-      borderRadius: '4px',
-    }
-    return (
-      <>
-        <div className='d-flex align-items-center justify-content-between'>
-          <div>
-            <TwitterPicker
-              triangle='hide'
-              colors={colors}
-              color={this.state.data.theme}
-              onChangeComplete={this.handleChangeComplete}
-              width='400px'
-            />
-          </div>
-          <div style={customColor} />
-        </div>
-      </>
-    )
-  }
-}
-
-export default CustomPicker(CustomColorPicker)
+export default CustomPicker(CustomColorPicker);
