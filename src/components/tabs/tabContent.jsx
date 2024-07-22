d1e73f11870b9fe26da34234847fc48c0491cc4b
shivm29 <developers@whozzat.com>
2024-07-22 17:49:50 +0530

    fixed some mistakes

--- src/components/tabs/tabContent.jsx
+++ src/components/tabs/tabContent.jsx
@@ -12,16 +12,6 @@ import { updateCollection } from '../collections/redux/collectionsActions';
 import PublishDocsReview from './../publishDocs/publishDocsReview';
 import { updateContent } from '../pages/redux/pagesActions';
 
-const withQuery = (WrappedComponent) => {
-  return (props) => {
-    const queryClient = useQueryClient();
-    const deleteFromReactQueryByKey = (id) => {
-      queryClient.removeQueries(['pageContent', id]);
-    };
-    return <WrappedComponent {...props} deleteFromReactQueryByKey={deleteFromReactQueryByKey} />;
-  };
-};
-
 const TabContent = ({ handle_save_endpoint, handle_save_page, save_endpoint_flag, save_page_flag, selected_tab_id }) => {
   const dispatch = useDispatch();
   const queryClient = useQueryClient();
@@ -131,4 +121,4 @@ const TabContent = ({ handle_save_endpoint, handle_save_page, save_endpoint_flag
   );
 };
 
-export default withQuery(TabContent);
+export default TabContent
