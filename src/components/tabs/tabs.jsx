13d729bfc3fbe5c293981ff38f341f6ebe7429f4
idris bohra <idrisbohra786@gmail.com>
2024-07-23 13:27:13 +0530

    hotfixed

--- src/components/tabs/tabs.jsx
+++ src/components/tabs/tabs.jsx
@@ -17,7 +17,7 @@ import { LuHistory } from 'react-icons/lu'
 import { GrGraphQl } from 'react-icons/gr'
 import Plus from '../../assets/icons/plus.svg'
 import { TbSettingsAutomation } from 'react-icons/tb'
-import { useNavigate, useParams } from 'react-router-dom'
+import { useLocation, useNavigate, useParams } from 'react-router-dom'
 import './tabs.scss'
 
 const CustomTabs = (props) => {
@@ -29,6 +29,7 @@ const CustomTabs = (props) => {
 
   const navigate = useNavigate()
   const params = useParams()
+  const location = useLocation()
 
   const [showSavePromptFor, setShowSavePromptFor] = useState([])
   const [leftScroll, setLeftScroll] = useState(0)
@@ -287,7 +288,7 @@ const CustomTabs = (props) => {
         break
       case 'collection': {
         const collectionName = collections[tabId]?.name || 'Collection'
-        if (props.location?.pathname?.split('/')?.[6] === 'settings') {
+        if (location?.pathname?.split('/')?.[6] === 'settings') {
           return (
             <>
               <span className='d-flex align-items-center'>
@@ -296,7 +297,7 @@ const CustomTabs = (props) => {
               </span>
             </>
           )
-        } else if (props.location?.pathname?.split('/')?.[6] === 'runner') {
+        } else if (location?.pathname?.split('/')?.[6] === 'runner') {
           return (
             <div className='d-flex align-items-center'>
               <TbSettingsAutomation size={18} className='setting-icons mr-1 mb-1' />
