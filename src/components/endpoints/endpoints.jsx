ec2198351bb16b4cb727adc49741bdb870489592
shivm29 <developers@whozzat.com>
2024-07-26 12:30:28 +0530

    changes in collections endpoints and tabs.jsx

--- src/components/endpoints/endpoints.jsx
+++ src/components/endpoints/endpoints.jsx
@@ -23,23 +23,31 @@ const Endpoints = (props) => {
   const [isHovered, setIsHovered] = useState(false)
   const [selectedEndpoint, setSelectedEndpoint] = useState(null)
 
-  const navigate = useNavigate()
   const params = useParams()
   const location = useLocation()
+  const navigate = useNavigate()
   const dispatch = useDispatch()
 
-  const { endpoints, tabs, pages, collections, } = useSelector((state) => ({
+  const { endpoints, tabs, pages, collections } = useSelector((state) => ({
     endpoints: state.pages,
     tabs: state.tabs,
     clientData: state.clientData,
     pages: state.pages,
-    collections: state.collections,
+    collections: state.collections
   }))
 
-  const handleDelete = async (endpoint) => {
-    await dispatch(deleteEndpoint(endpoint))
+  const handleDelete = (endpoint) => {
+    dispatch(deleteEndpoint(endpoint))
     tabService.removeTab(tabs.activeTabId, { navigate, params, location })
   }
+
+  const handleModalActionType = (actionType, endpointId) => {
+    setShowEndpointForm((prev) => ({ ...prev, [actionType]: true }))
+    setSelectedEndpoint(endpoints[endpointId])
+  }
+
+  const handleDuplicate = (endpointId) => dispatch(duplicateEndpoint(endpoints[endpointId]))
+
   const closeDeleteEndpointModal = () => {
     setShowEndpointForm((prev) => ({ ...prev, delete: false }))
   }
@@ -50,14 +58,7 @@ const Endpoints = (props) => {
       if (!tabs.tabs[endpoint.id]) {
         const previewTabId = Object.keys(tabs.tabs).filter((tabId) => tabs.tabs[tabId].previewMode === true)[0]
         if (previewTabId) dispatch(closeTab(previewTabId))
-        dispatch(openInNewTab({
-          id: endpoint.id,
-          type: 'endpoint',
-          status: tabStatusTypes.SAVED,
-          previewMode,
-          isModified: false,
-          state: {},
-        }))
+        dispatch(openInNewTab({ id: endpoint.id, type: 'endpoint', status: tabStatusTypes.SAVED, previewMode, isModified: false, state: {} }))
       } else if (tabs.tabs[endpoint.id].previewMode === true && previewMode === false) {
         tabService.disablePreviewMode(endpoint.id)
       }
@@ -78,17 +79,14 @@ const Endpoints = (props) => {
   }
 
   const displayEndpointName = (endpointId) => {
-    const isSelected =
-      isOnPublishedPage() && sessionStorage.getItem('currentPublishIdToShow') === endpointId
-        ? 'selected'
-        : isDashboardRoute({ location, navigate }) && params.endpointId === endpointId
-          ? 'selected'
-          : ''
+    const isSelected = isOnPublishedPage() && sessionStorage.getItem('currentPublishIdToShow') === endpointId   ? 'selected'   : isDashboardRoute({ location, navigate }) && params.endpointId === endpointId   ? 'selected'   : ''
     return (
       <div className={`sidebar-accordion-item ${isSelected ? 'Selected' : ''}`}>
-        {endpoints[endpointId]?.protocolType === 1 && <div className={`api-label ${endpoints[endpointId].requestType} request-type-bgcolor ${!isOnPublishedPage() ? 'in-api-label' : ''}`}>
-          {endpoints[endpointId].requestType}
-        </div>}
+        {endpoints[endpointId]?.protocolType === 1 && (
+          <div className={`api-label ${endpoints[endpointId].requestType} request-type-bgcolor ${!isOnPublishedPage() ? 'in-api-label' : ''}`}>
+            {endpoints[endpointId].requestType}
+          </div>
+        )}
         {endpoints[endpointId]?.protocolType === 2 && <GrGraphQl className='mr-2 graphql-icon' size={14} />}
         <div className='end-point-name truncate'>{endpoints[endpointId].name}</div>
       </div>
@@ -103,33 +101,16 @@ const Endpoints = (props) => {
         </IconButtons>
       </div>
       <div className='dropdown-menu dropdown-menu-right'>
-        <div className='dropdown-item d-flex' onClick={() => {
-          setShowEndpointForm((prev) => ({ ...prev, edit: true }))
-          setSelectedEndpoint(endpoints[endpointId])
-        }}>
-          <RenamedItem /> Rename
-        </div>
-        <div className='dropdown-item d-flex' onClick={() => dispatch(duplicateEndpoint(endpoints[endpointId]))}>
-          <Duplicate /> Duplicate
-        </div>
-        <div className='dropdown-item text-danger d-flex' onClick={() => {
-          setShowEndpointForm((prev) => ({ ...prev, delete: true }))
-          setSelectedEndpoint(endpoints[endpointId])
-        }}>
-          <DeleteIcon /> Delete
-        </div>
+        <div className='dropdown-item d-flex' onClick={()=>handleModalActionType('edit', endpointId)}> <RenamedItem /> Rename </div>
+        <div className='dropdown-item d-flex' onClick={() => handleDuplicate(endpointId)}> <Duplicate /> Duplicate </div>
+        <div className='dropdown-item d-flex text-danger' onClick={()=>handleModalActionType('delete', endpointId)}> <DeleteIcon /> Delete </div>
       </div>
     </div>
   )
 
   const displaySingleEndpoint = (endpointId) => {
     let isUserOnPublishedPage = isOnPublishedPage()
-    const isSelected =
-      isUserOnPublishedPage && sessionStorage.getItem('currentPublishIdToShow') === endpointId
-        ? 'selected'
-        : isDashboardRoute({ location, navigate }) && params.endpointId === endpointId
-          ? 'selected'
-          : ''
+    const isSelected = isUserOnPublishedPage && sessionStorage.getItem('currentPublishIdToShow') === endpointId ? 'selected' : isDashboardRoute({ location, navigate }) && params.endpointId === endpointId ? 'selected' : ''
     let idToRender = sessionStorage.getItem(SESSION_STORAGE_KEY.CURRENT_PUBLISH_ID_SHOW)
     let collectionId = pages?.[idToRender]?.collectionId ?? null
     var collectionTheme = collections[collectionId]?.theme
@@ -137,14 +118,10 @@ const Endpoints = (props) => {
     const staticColor = background['background_hover']
 
     const backgroundStyle = {
-      backgroundImage:
-        isHovered || isSelected
-          ? `linear-gradient(to right, ${dynamicColor}, ${dynamicColor}),
-        linear-gradient(to right, ${staticColor}, ${staticColor})`
-          : ''
+      backgroundImage: isHovered || isSelected ? `linear-gradient(to right, ${dynamicColor}, ${dynamicColor}), linear-gradient(to right, ${staticColor}, ${staticColor})` : ''
     }
+
     return (
-      <>
         <div
           key={endpointId}
           draggable={!isUserOnPublishedPage}
@@ -157,34 +134,22 @@ const Endpoints = (props) => {
         >
           <div className='sidebar-toggle d-flex justify-content-between mt-1'>
             <button>
-              <div
-                className={`side-bar d-flex align-items-center rounded mr-2 ${isSelected ? 'Selected' : ''}`}
-                style={backgroundStyle}
-                onMouseEnter={() => setIsHovered(true)}
-                onMouseLeave={() => setIsHovered(false)}
-              >
-                <button
-                  tabIndex={-1}
-                  onClick={() => handleDisplay(endpoints[endpointId], params.endpointId, collectionId, true)}
-                  onDoubleClick={() => handleDisplay(endpoints[endpointId], params.endpointId, collectionId, false)}
-                >
+              <div className={`side-bar d-flex align-items-center rounded mr-2 ${isSelected ? 'Selected' : ''}`} style={backgroundStyle} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+                <button tabIndex={-1} onClick={() => handleDisplay(endpoints[endpointId], true)} onDoubleClick={() => handleDisplay(endpoints[endpointId], false)}>
                   {displayEndpointName(endpointId)}
                 </button>
                 <div className='d-flex align-items-center'>
-                  {isDashboardRoute({ navigate, location }, true) &&
-                    !collections[collectionId]?.importedFromMarketPlace &&
-                    displayEndpointOptions(endpointId)}
+                  {isDashboardRoute({ navigate, location }, true) && displayEndpointOptions(endpointId)}
                 </div>
               </div>
             </button>
           </div>
         </div>
-      </>
     )
   }
 
   return (
-    <>
+    <React.Fragment>
       {showEndpointForm.edit && (
         <SubPageForm
           {...props}
@@ -198,17 +163,9 @@ const Endpoints = (props) => {
           selectedPage={selectedEndpoint?.id}
         />
       )}
-      {showEndpointForm.delete &&
-        endpointService.showDeleteEndpointModal(
-          { tabs, dispatch },
-          handleDelete,
-          closeDeleteEndpointModal,
-          'Delete Endpoint',
-          `Are you sure you want to delete this endpoint?`,
-          selectedEndpoint
-        )}
+      {showEndpointForm.delete && endpointService.showDeleteEndpointModal({ tabs, dispatch },handleDelete,closeDeleteEndpointModal,'Delete Endpoint',`Are you sure you want to delete this endpoint?`,selectedEndpoint)}
       {displaySingleEndpoint(props.endpointId)}
-    </>
+    </React.Fragment>
   )
 }
 export default Endpoints
