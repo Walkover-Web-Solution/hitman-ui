524688782ddfdc11fa9030f0fbc7ad411e4d6d8b
idris bohra <idrisbohra786@gmail.com>
2024-08-01 11:40:18 +0530

    done with the chatbot

--- src/components/pages/displayPage.jsx
+++ src/components/pages/displayPage.jsx
@@ -129,13 +129,13 @@ class DisplayPage extends Component {
     }
   }
 
-  componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate(prevProps) {
     const userid = getCurrentUser()?.id
     if (typeof window.SendDataToChatbot === 'function' && this.props?.tabs?.tabs[this.props?.tabs?.activeTabId]?.type === 'page') {
       window.SendDataToChatbot({
         bridgeName: 'page',
-        threadId: `${userid}`,
-        variables: { Proxy_auth_token: getProxyToken(), content: this.props.pageContent, collectionId: this.props.pages[this.props?.params?.pageId]?.collectionId}
+        threadId: `${userid}-${this.props?.params?.pageId}`,
+        variables: { Proxy_auth_token: getProxyToken(), collectionId: this.props.pages[this.props?.params?.pageId]?.collectionId }
       })
     }
     if (this.props?.location?.pathname !== prevProps?.location?.pathname) {
@@ -226,7 +226,7 @@ class DisplayPage extends Component {
   }
 
   renderTiptapEditor(contents) {
-    return <Tiptap onChange={() => {}} initial={contents} isInlineEditor disabled key={Math.random()} />
+    return <Tiptap onChange={() => { }} initial={contents} isInlineEditor disabled key={Math.random()} />
   }
 
   handleRemovePublicPage(page) {
