e5db71293a5575db15d233e4fdb746e7bcdb86fc
idris bohra <idrisbohra786@gmail.com>
2024-08-03 16:23:50 +0530

    style changes

--- src/components/pages/hoverBox/hoverBox.jsx
+++ src/components/pages/hoverBox/hoverBox.jsx
@@ -7,17 +7,11 @@ export default function HoverBox(props) {
             {isOnPublishedPage() && props?.headings.length > 0 && (
                 <div className='heading-main position-fixed'>
                     <div className='editor-headings p-2 rounded-sm position-fixed d-flex flex-column'>
-                        <div className='h-100'>
-                            <div>
-                                {props?.headings.map((heading) => (
-                                    <div key={heading.id}>
-                                        <span onClick={() => props?.scrollToHeading(heading.id)} className='d-block w-100 py-1 cursor-pointer'>
-                                            {heading.text}
-                                        </span>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
+                        {props?.headings.map((heading) => (
+                            <span onClick={() => props?.scrollToHeading(heading.id)} className='d-block w-100 p-1 cursor-pointer'>
+                                {heading.text}
+                            </span>
+                        ))}
                     </div>
                 </div>
             )}
