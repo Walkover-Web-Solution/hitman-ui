da4924a8ea29afc6f8672cc79976c6e1abad95df
Viasocket Interns <developers@whozzat.com>
2024-08-20 11:21:19 +0530

    Riya/doc 75/evironment prod (#518)
    * environment type added
    
    * autocomplete changes
    
    * <> url changed
    
    * ui
    
    * env type added
    
    * delete environment handled
    
    * ui changes
    
    * autocomplete }} added in the end
    
    * run automation
    
    * react query data updated
    
    * sidebar line removed
    
    * export environment done
    
    * ui changes
    
    * changes
    
    ---------
    
    Co-authored-by: Riya Bansal <riyabansalofficial2814@gmail.com>
    Co-authored-by: Swayam Maheshwari <swayammaheshwari890@gmail.com>

--- src/components/environments/environments.jsx
+++ src/components/environments/environments.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Dropdown } from 'react-bootstrap'
+import { Dropdown, OverlayTrigger, Tooltip } from 'react-bootstrap'
 import EnvironmentModal from './environmentModal.jsx'
 import { ReactComponent as EyeIcon } from '../../assets/icons/eye.svg'
 import { ReactComponent as EyeDisabledIcon } from '../../assets/icons/eyeDisabled.svg'
@@ -11,6 +11,11 @@ import { fetchEnvironments, fetchEnvironmentsFromLocalStorage, setEnvironmentId 
 import EnvironmentVariables from './environmentVariables.jsx'
 import DeleteModal from '../common/deleteModal.jsx'
 import './environments.scss'
+import { FaCheck } from "react-icons/fa6";
+import { getCurrentUser } from '../auth/authServiceV2.js'
+import { BiExport } from 'react-icons/bi'
+import exportEnvironmentApi from './exportEnvironmentApi.js'
+import { FaGlobeAmericas } from 'react-icons/fa'
 
 const Environments = () => {
 
@@ -23,6 +28,7 @@ const Environments = () => {
   })
 
   const dispatch = useDispatch()
+  const userId = getCurrentUser()?.id
 
   const [environmentFormName, setEnvironmentFormName] = useState('')
   const [environmentToBeEdited, setEnvironmentToBeEdited] = useState(null)
@@ -53,6 +59,28 @@ const Environments = () => {
     setShowDeleteModal(false)
   }
 
+  const showTooltips = () => {
+    return <Tooltip className="fs-4 text-secondary"><span >Export environment</span></Tooltip>
+  }
+
+  const handleExport = async (Id) => {
+    try {
+        const data = await exportEnvironmentApi(Id, organizations?.currentOrg.id);
+        const blob = new Blob([JSON.stringify(data, null, 2)], { type: 'application/json' });
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', `${data.name || 'environment'}.json`);
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        URL.revokeObjectURL(url);
+    } catch (error) {
+        console.error('Error exporting data:', error);
+    }
+};
+
+
   const handleEnv = (environmentId) => {
     dispatch(setEnvironmentId(environmentId))
   }
@@ -79,14 +107,20 @@ const Environments = () => {
 
         <div className='select-environment-dropdown border-radius-right-none align-content-center'>
           <Dropdown className='ml-1'>
-          <IconButton variant='sm'>
-            <Dropdown.Toggle className='p-0 pl-1' variant='default' id='dropdown-basic'>
+          <IconButton variant='sm'><Dropdown.Toggle className='p-0 pl-1' variant='default' id='dropdown-basic'>
               <span className='truncate'>{environment?.environments[environment?.currentEnvironmentId] ? environment.environments[environment.currentEnvironmentId].name : 'No Environment'}</span>
-             <IoIosArrowDown className='m-1' />
+              <IoIosArrowDown className='m-1' />
             </Dropdown.Toggle></IconButton>
             <Dropdown.Menu alignRight>
               <Dropdown.Item onClick={() => handleEnv(null)} key='no-environment'>No Environment</Dropdown.Item>
-              {Object.keys(environment.environments).map((environmentId) => <Dropdown.Item onClick={() => handleEnv(environmentId)} key={environmentId}>{environment.environments[environmentId]?.name}</Dropdown.Item>)}
+              {Object.keys(environment.environments).map((environmentId) => <Dropdown.Item onClick={() => handleEnv(environmentId)} key={environmentId}>
+                {environment.environments[environmentId]?.name}
+                {environmentId === currentEnvironmentId && <span className='check-icon'><FaCheck /></span>}
+                <OverlayTrigger placement="bottom" overlay={showTooltips()} >
+                  <span className='export-icon' onClick={(event) => { event.stopPropagation();handleExport(environment?.environments[environmentId].id)}}><BiExport size={18} /></span>
+                </OverlayTrigger>
+                {environment?.environments[environmentId]?.userId === 0 && environment?.environments[environmentId]?.orgId !== null && environmentId !== currentEnvironmentId && <span className='global-icon'><FaGlobeAmericas /></span>}
+              </Dropdown.Item>)}
               <Dropdown.Divider />
               <Dropdown.Item className='dropdown-item' onClick={() => handleEnvironmentModal('Add new Environment')}>Add Environment</Dropdown.Item>
               <Dropdown.Item className='dropdown-item' onClick={() => setShowImportModal(true)}>Import Environment</Dropdown.Item>
@@ -96,7 +130,6 @@ const Environments = () => {
         </div>
         <ImportEnvironmentModal show={showImportModal} onClose={() => setShowImportModal(false)} />
       </div>
-
     )
   }
 }
