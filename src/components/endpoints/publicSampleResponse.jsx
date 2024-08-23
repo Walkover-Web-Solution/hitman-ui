18f086ef60b7b1951442081da498b992bc0eb85c
Riya Bansal <riyabansalofficial2814@gmail.com>
2024-08-23 11:23:21 +0530

    Riya| expand-collapse, status buttons

--- src/components/endpoints/publicSampleResponse.jsx
+++ src/components/endpoints/publicSampleResponse.jsx
@@ -5,7 +5,7 @@ import { willHighlight, getHighlightsData } from './highlightChangesHelper'
 import './endpoints.scss'
 import { Style } from 'react-style-tag'
 import { hexToRgb } from '../common/utility'
-import {background} from '../backgroundColor.js'
+import { background } from '../backgroundColor.js'
 class PublicSampleResponse extends Component {
   constructor(props) {
     super(props);
@@ -14,11 +14,37 @@ class PublicSampleResponse extends Component {
         publicCollectionTheme: this.props.publicCollectionTheme,
         backgroundStyle: {}
       },
+      isExpanded: false,
+      maxHeight: 300,
+      showExpandButton: false,
     };
+    this.contentRef = React.createRef();
+    this.toggleExpand = this.toggleExpand.bind(this);
   }
+
   componentDidMount() {
     this.updateBackgroundStyle();
+    this.checkContentHeight();
   }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.sample_response_array !== this.props.sample_response_array || prevState.isExpanded !== this.state.isExpanded) {
+      this.checkContentHeight();
+    }
+  }
+
+  checkContentHeight() {
+    if (this.contentRef.current) {
+      const contentHeight = this.contentRef.current.scrollHeight;
+      const { maxHeight } = this.state;
+      const shouldShowExpandButton = contentHeight > maxHeight;
+
+      if (shouldShowExpandButton !== this.state.showExpandButton) {
+        this.setState({ showExpandButton: shouldShowExpandButton });
+      }
+    }
+  }
+
   updateBackgroundStyle() {
     const { publicCollectionTheme } = this.state.theme;
     const dynamicColor = hexToRgb(publicCollectionTheme, 0.02);
@@ -55,7 +81,17 @@ class PublicSampleResponse extends Component {
     }
   }
 
+  toggleExpand() {
+    this.setState(prevState => ({ isExpanded: !prevState.isExpanded }));
+  }
+
   render() {
+    const { isExpanded, maxHeight, showExpandButton } = this.state;
+    const contentStyle = {
+      maxHeight: isExpanded ? 'none' : `${maxHeight}px`,
+      overflow: 'hidden',
+      position: 'relative',
+    };
     return (
       <>
         <Style>
@@ -70,29 +106,35 @@ class PublicSampleResponse extends Component {
         <div className='pubSampleResponse'>
           <div className='heading-2 pt-1 mt-4 font-16 mb-2'>
             <span>Sample Response {willHighlight(this.props, 'sampleResponse') ? <i className='fas fa-circle' /> : null}</span>
-          </div>
-          <div className='sample-response mb-1' style={this.state.theme.backgroundStyle}>
-            <Tabs id='uncontrolled-tab-example' aria-hidden="true">
+            <Tabs id='uncontrolled-tab-example' aria-hidden="true" >
               {this.props.sample_response_array.map((sampleResponse, key) => (
                 <Tab
-                key={key}
-                  eventKey={sampleResponse.title}
+                  key={key}
+                  eventKey={sampleResponse.status}
                   title={
-                    getHighlightsData(this.props, 'sampleResponse', sampleResponse.title) ? (
+                    getHighlightsData(this.props, 'sampleResponse', sampleResponse.status) ? (
                       <span>
-                        {sampleResponse.title}
+                        {sampleResponse.status}
                         <i className='fas fa-circle' />
                       </span>
                     ) : (
-                      sampleResponse.title
+                      sampleResponse.status
                     )
                   }
                 >
-                  <div>{sampleResponse.status}</div>
-                  <div>{sampleResponse.description}</div>
-                  <div>{this.showSampleResponseBody(sampleResponse.data)}</div>
+                  <div className='sample-response mb-1' style={this.state.theme.backgroundStyle}>
+                    <div ref={this.contentRef}  style={contentStyle}>
+                      <div>{sampleResponse.description}</div>
+                      <div>{this.showSampleResponseBody(sampleResponse.data)}</div>
+                    </div>
+                    {showExpandButton && (
+                      <div className="expand-btn " onClick={this.toggleExpand}>
+                        {isExpanded ? 'Show Less' : 'Show More'}
+                      </div>
+                    )}
+                  </div>
                 </Tab>
-              ))}
+              ))} 
             </Tabs>
           </div>
         </div>
