a7c25056d105167fd595a19b565c1fa95ec1bb68
idris bohra <idrisbohra786@gmail.com>
2024-09-12 13:44:21 +0530

    changes

---
+++ src/components/shortcutModal/shortcutModal.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Button, Modal } from 'react-bootstrap';
+import { RxCross2 } from "react-icons/rx";
+import IconButton from '../common/iconButton';
+import { BsCommand } from 'react-icons/bs';
+
+const ShortcutModal = ({ hideModal }) => {
+    const isMac = navigator.platform.toUpperCase().indexOf('MAC') >= 0;
+
+    const shortcuts = [
+        { name: 'Save', key: isMac ?  [<BsCommand key="cmd"/>, 'S'] : ['Ctrl', 'S'] }, 
+        { name: 'Open Tab', key: isMac ? ['Ctrl', 'N'] : ['Alt', 'N'] },
+        { name: 'Publish', key: isMac ? [<BsCommand  key="cmd"/>, 'B']  : ['Ctrl', 'B'] },
+        { name: 'Close Tab', key: isMac ? ['Ctrl', 'W'] : ['Alt', 'W'] },
+        { name: 'Unpublish', key: isMac ? [<BsCommand key="cmd"/>, 'U']  : ['Ctrl', 'U'] },
+        { name: 'Switch Tab', key: isMac ? ['Ctrl', 'T'] : ['Alt', 'T'] },
+        { name: 'Show Shortcuts', key: isMac ? [<BsCommand key="cmd"/>, '/']  :['Ctrl', '/'] },
+
+    ];
+
+    return (
+        <div className="modal-container" >
+            <div className="modal-header">
+                <h2>Keyboard Shortcuts</h2>
+                <Button onClick={hideModal} className="close-btn">
+                    <IconButton>
+                        <RxCross2 />
+                    </IconButton>
+                </Button>
+            </div>
+            <Modal.Body className="modal-body">
+                <ul >
+                    {shortcuts.map((shortcut, index) => (
+                        <li key={index}>
+                            <span >{shortcut.name}</span>
+                            <span className="shortcut-key">
+                                {shortcut.key.map((keyPart, idx) => (
+                                    <span key={idx} className="key-box ">{keyPart}</span>
+                                ))}
+                            </span>
+                        </li>
+                    ))}
+                </ul>
+            </Modal.Body>
+        </div>
+    );
+};
+
+export default ShortcutModal;
