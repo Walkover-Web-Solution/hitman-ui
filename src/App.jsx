e499bcc17944a1730cbe7ee548fc0699fae04f76
goutam01mehta <88901454+goutam01mehta@users.noreply.github.com>
2024-09-27 14:04:27 +0530

    unregistered service worker

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,15 @@ const App = () => {
   const dispatch = useDispatch()
 
   useEffect(() => {
-
+    function unregisterServiceWorker() {
+      if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.ready.then((registration) => {
+          registration.unregister();
+          console.log("unregistered")
+        });
+      }
+    }
+    unregisterServiceWorker();
     const scriptId = "chatbot-main-script"
     const chatbot_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmdfaWQiOiI1OTgyIiwiY2hhdGJvdF9pZCI6IjY2NTQ3OWE4YmQ1MDQxYWU5M2ZjZDNjNSIsInVzZXJfaWQiOiIxMjQifQ.aI4h6OmkVvQP5dyiSNdtKpA4Z1TVNdlKjAe5D8XCrew"
     const scriptSrc = "https://chatbot-embed.viasocket.com/chatbot-prod.js"
@@ -43,6 +51,8 @@ const App = () => {
       document.head.appendChild(script);
       script.src = scriptSrc
     }
+
+
 
     const currentOrgId = getOrgId() ?? window.location.pathname.split('/')?.[2]
     if (currentOrgId && !isOnPublishedPage()) {
