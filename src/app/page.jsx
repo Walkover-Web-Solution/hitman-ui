56adcca2e76aee3e3d162b4c5fd66f4083e124af
idris bohra <idrisbohra786@gmail.com>
2024-10-01 17:09:14 +0530

    new change

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,20 +1,10 @@
-// import IndexWebsite from '@/components/indexWebsite/indexWebsite'
 import React from 'react'
-
-// function page() {
-//   return (
-//     <IndexWebsite />
-//     // <div>page</div>
-//   )
-// }
-
-// export default page
 import Layout from './layout';
 
 export default function Home() {
   return (
-    <Layout>
-      {/* Your page content goes here */}
-    </Layout>
+    <div>
+      Hello world
+    </div>
   );
 }
