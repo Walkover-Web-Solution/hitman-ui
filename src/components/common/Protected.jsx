8aad0e1554a982b6bf68e1823713fd8dd630d7be
goutam01mehta <88901454+goutam01mehta@users.noreply.github.com>
2024-10-02 17:13:58 +0530

    add protected route

---
+++ src/components/common/Protected.jsx
@@ -0,0 +1,24 @@
+"use-client"
+
+import { useRouter } from "next/navigation";
+import React, { useEffect } from 'react';
+
+const Protected = (WrappedComponent) => {
+  return (props) => {
+    const router = useRouter();
+
+    useEffect(() => {
+localStorageGet()
+    }, []);
+
+    const localStorageGet = () =>{
+      if (typeof window !== 'undefined' && !localStorage.getItem("proxy_token")) {
+        router.replace('/');
+        return null;
+      }
+    }
+    return <WrappedComponent {...props} />;
+  };
+};
+
+export default Protected;
