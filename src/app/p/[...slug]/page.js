a84ccefe200485f3bc71bee985fb64c338da13e2
idris bohra <idrisbohra786@gmail.com>
2024-10-05 17:35:29 +0530

    temp

--- src/app/p/[...slug]/page.js
+++ src/app/p/[...slug]/page.js
@@ -5,18 +5,12 @@ export default async function Page({ params, searchParams }) {
     const apiUrl = process.env.NEXT_PUBLIC_API_URL;
     const queryParams = searchParams;
     const slug = params.slug;
-    console.log('Query Params:', queryParams);
-    console.log('Path:', slug);
-
     let queryParamApi2 = {};
-
     queryParamApi2.collectionId = searchParams.collectionId;
-    queryParamApi2.path = slug.join('/'); // Exclude the first element
-
+    queryParamApi2.path = slug.join('/');
     if (queryParams.version) {
         queryParamApi2.versionName = queryParams.version;
     }
-
     let queryParamsString = '?';
     for (let key in queryParamApi2) {
         if (queryParamApi2.hasOwnProperty(key)) {
@@ -25,21 +19,15 @@ export default async function Page({ params, searchParams }) {
     }
     queryParamsString = queryParamsString.slice(0, -1);
     const response = await fetch(apiUrl + `/getPublishedDataByPath${queryParamsString}`);
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-const data = await response.json(); 
-// Now you can use `data` as needed
-console.log(data);
-
-
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
 
     return (
         <div>
             <Providers>
-                <PublicEndpoint />
+                <PublicEndpoint pageContentDataSSR={data?.publishedContent?.publishedPage || ''} />
             </Providers>
         </div>
     );
