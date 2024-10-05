e0337f7ca4a968dd06e3381a48f507492defadb0
Walk-MAC <walk-mac@Prince.local>
2024-10-05 16:50:39 +0530

    added logic of getting data by path

--- src/app/p/[...slug]/page.js
+++ src/app/p/[...slug]/page.js
@@ -1,18 +1,40 @@
-import React from 'react';
-import PublicEndpoint from '../../../components/publicEndpoint/publicEndpoint'
-import Providers from '../../providers/providers';
-
+import PublicEndpoint from "@/components/publicEndpoint/publicEndpoint";
+import Providers from "src/app/providers/providers";
 
 export default async function Page({ params, searchParams }) {
-    const apiUrl = process.env.NEXT_PUBLIC_API_URL
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL;
     const queryParams = searchParams;
-    const slug = params
+    const slug = params.slug;
     console.log('Query Params:', queryParams);
     console.log('Path:', slug);
-    const queryParamsString = '?collectionId=YecLEi9wSYig&public=true&path=sms/sms-logs'
-    const response = await fetch(apiUrl + `/getPublishedDataByPath${queryParamsString}`)
-    const data = await response.json();
-    console.log(data)
+
+    let queryParamApi2 = {};
+
+    queryParamApi2.collectionId = searchParams.collectionId;
+    queryParamApi2.path = slug.join('/'); // Exclude the first element
+
+    if (queryParams.version) {
+        queryParamApi2.versionName = queryParams.version;
+    }
+
+    let queryParamsString = '?';
+    for (let key in queryParamApi2) {
+        if (queryParamApi2.hasOwnProperty(key)) {
+            queryParamsString += `${encodeURIComponent(key)}=${encodeURIComponent(queryParamApi2[key])}&`;
+        }
+    }
+    queryParamsString = queryParamsString.slice(0, -1);
+    const response = await fetch(apiUrl + `/getPublishedDataByPath${queryParamsString}`);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+const data = await response.json(); 
+// Now you can use `data` as needed
+console.log(data);
+
+
 
     return (
         <div>
