a121c7f9b11f0b423e5b525cc25dccaecaec294f
idris bohra <idrisbohra786@gmail.com>
2024-10-05 16:09:30 +0530

    changes

--- src/app/p/[...slug]/page.js
+++ src/app/p/[...slug]/page.js
@@ -1,5 +1,21 @@
-import React from 'react'
+import React from 'react';
+import PublicEndpoint from '../../../components/publicEndpoint/publicEndpoint'
+import Providers from '../../providers/providers';
+import generalApiService from '../../../services/generalApiService';
 
-export default function Page() {
-    return null;
+
+export default async function Page({ params, searchParams }) {
+    const queryParams = searchParams // Access query parameters
+    const slug = params
+    console.log('Query Params:', queryParams);
+    console.log('Path:', slug);
+    // const response = await generalApiService.getPublishedContentByPath(queryParamsString)
+
+    return (
+        <div>
+            <Providers>
+                <PublicEndpoint />
+            </Providers>
+        </div>
+    );
 }
--- src/app/p/page.jsx
+++ src/app/p/page.jsx
@@ -1,5 +1,16 @@
-import React from 'react'
+import React from 'react';
+import PublicEndpoint from '../../../components/publicEndpoint/publicEndpoint'
+import Providers from '../../providers/providers';
 
-export default function Page() {
-    return null;
+
+export default async function Page({ params, searchParams }) {
+    const queryParams = searchParams
+    console.log('Query Params:', queryParams);
+    return (
+        <div>
+            <Providers>
+                <PublicEndpoint />
+            </Providers>
+        </div>
+    );
 }
