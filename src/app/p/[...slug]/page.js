ef276aee9bfdafa5f171eb64f44cb5c23d0678da
idris bohra <idrisbohra786@gmail.com>
2024-10-05 16:18:14 +0530

    changes

--- src/app/p/[...slug]/page.js
+++ src/app/p/[...slug]/page.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import PublicEndpoint from '../../../components/publicEndpoint/publicEndpoint'
 import Providers from '../../providers/providers';
-import generalApiService from '../../../services/generalApiService';
 
 
 export default async function Page({ params, searchParams }) {
-    const queryParams = searchParams // Access query parameters
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL
+    const queryParams = searchParams;
     const slug = params
     console.log('Query Params:', queryParams);
     console.log('Path:', slug);
-    // const response = await generalApiService.getPublishedContentByPath(queryParamsString)
+    const queryParamsString = '?collectionId=YecLEi9wSYig&public=true'
+    // const response = await generalApiService.getPublishedContentByPath('collectionId=YecLEi9wSYig')
+    const response = await fetch(apiUrl + `/getPublishedDataByPath${queryParamsString}`)
+    console.log(response)
 
     return (
         <div>
