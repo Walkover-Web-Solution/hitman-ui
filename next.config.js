1f21f34ef9d31349f9a9602fef19fc758301981c
walkover-ankit <ankitkumar@whozzat.com>
2024-10-08 18:04:24 +0530

    Deployment changes

--- next.config.js
+++ next.config.js
@@ -1,18 +1,19 @@
-   // next.config.js
-   const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANALYZE === 'true',
-   });
+// next.config.js
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 
-   module.exports = withBundleAnalyzer({
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ["@svgr/webpack"],
-      });
+module.exports = withBundleAnalyzer({
+  output: 'standalone',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
 
-      return config;
-    },
-   });
+    return config;
+  },
+});
