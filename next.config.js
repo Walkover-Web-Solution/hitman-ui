ae2add7581ada3b587fdea1a5933d4dd8b3b22e4
idris bohra <idrisbohra786@gmail.com>
2024-10-15 20:11:19 +0530

    next.config.js changes

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ module.exports = withBundleAnalyzer({
   eslint: {
     ignoreDuringBuilds: true,
   },
+  assetPrefix: process.env.NEXT_PUBLIC_UI_URL,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
