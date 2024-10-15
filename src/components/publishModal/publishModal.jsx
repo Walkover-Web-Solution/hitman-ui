820dc5eb0e1598217da11f840250b80d50983640
Riya Bansal <riyabansalofficial2814@gmail.com>
2024-10-15 17:54:03 +0530

    redux of meta tags and description

--- src/components/publishModal/publishModal.jsx
+++ src/components/publishModal/publishModal.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Button, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import './publishModal.scss'
-import { useParams } from 'react-router';
 import { getUrlPathById, SESSION_STORAGE_KEY } from '../common/utility';
 import { useSelector } from 'react-redux';
 import { GoLink } from "react-icons/go";
@@ -9,7 +8,7 @@ import CopyToClipboard from 'react-copy-to-clipboard';
 import { toast } from 'react-toastify';
 import TagInput from './tagInput';
 
-function PublishModal({ onPublish, onUnpublish, id, collectionId, isContentChanged }) {
+function PublishModal({ onPublish, onUnpublish, id, collectionId, isContentChanged,queryData}) {
 
     const [disabledValue, setDisabledValue] = useState(null);
     const [save, setSave] = useState(true)
@@ -148,7 +147,7 @@ function PublishModal({ onPublish, onUnpublish, id, collectionId, isContentChang
                         </div>
                     </div>
                 )}
-               {isPublished && <TagInput /> } 
+               {isPublished && <TagInput pageId={id}/> } 
             </div>
             <div className='d-flex align-items-center justify-content-end gap-2 mt-4 '>
                 {!isPublished ? <Button className="cursor-pointer btn-sm font-12" onClick={handlePublishClick}>
