2ca4790fe0d5b2f1d43c14e452bf06b3c5902ca3
ViaSocket-Git <developers@whozzat.com>
2024-10-17 12:58:43 +0530

    Improvements and alignment adjustments for the public page

--- src/app/p/[...slug]/page.jsx
+++ src/app/p/[...slug]/page.jsx
@@ -101,11 +101,11 @@ export default async function Page({ params, searchParams, customDomain }) {
 
     return (
         <>
-            <div>
-                {content?.defaultHeader !== '' && <div className='navbar-public position-sticky top-0'>
+           <div className="top-container">
+                {content?.defaultHeader !== '' && <div className='navbar-public'>
                     <div className='preview-content mx-auto' dangerouslySetInnerHTML={{ __html: content?.defaultHeader ?? '' }} />
                 </div>}
-                {!data.error ? <div className="main-public-container d-flex max-width-container mx-auto">
+                {!data.error ? <div className="main-public-container d-flex max-width-container mx-auto min-h-100vh">
                     <Providers>
                         <PublicEndpoint />
                     </Providers>
