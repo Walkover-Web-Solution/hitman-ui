67a40ee15a90688918a5edd2e53e0cecb5d8dd94
Shivani <shivani@walkover.in>
2024-10-23 13:34:07 +0530

    <1> responsive public page

--- src/components/pages/hoverBox/hoverBox.jsx
+++ src/components/pages/hoverBox/hoverBox.jsx
@@ -1,5 +1,6 @@
 'use client'
 import React, { useEffect, useState } from 'react';
+import './hoverBox.scss';
 
 export default function HoverBox({ html }) {
     const [headings, setHeadings] = useState([]);
