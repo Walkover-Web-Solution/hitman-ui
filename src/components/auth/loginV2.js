62a2424978a78cb011654e03e56a3c7f6552a617
goutam01mehta <88901454+goutam01mehta@users.noreply.github.com>
2024-10-24 18:26:18 +0530

    solved the close tab, ui break on login and routing bugs

--- src/components/auth/loginV2.js
+++ src/components/auth/loginV2.js
@@ -63,34 +63,30 @@ const LoginV2 = () => {
   const divId = proxyGooglereferenceMapping[env];
 
   return (
-    <html>
-      <body>
-        <div className="login d-flex gap-sm-0 gap-4 flex-column-reverse flex-sm-row p-2 p-sm-0">
-          <div className="login__details deatil-sec col-xl-3 col-lg-4 col-md-5 col-sm-6 p-2 p-sm-4 p-xl-5">
-            <TECHDOC className="d-none d-sm-block" />
-            <h4 className="mt-4">
-              Your companys technical knowledge deserves a beautiful home
-            </h4>
-            <ul className="feature-list mt-3">
-              <li>Create-Collaborate-Review-Secure-Publish-Maintain</li>
-              <li>Create a source of truth</li>
-              <li>Your content, your way</li>
-              <li>Create/Manage Environment</li>
-              <li>Create/Share internal API documentation</li>
-              <li>Scalability and Performance</li>
-            </ul>
-          </div>
+    <div className="login d-flex gap-sm-0 gap-4 flex-column-reverse flex-sm-row p-2 p-sm-0">
+      <div className="login__details deatil-sec col-xl-3 col-lg-4 col-md-5 col-sm-6 p-2 p-sm-4 p-xl-5">
+        <TECHDOC className="d-none d-sm-block" />
+        <h4 className="mt-4">
+          Your companys technical knowledge deserves a beautiful home
+        </h4>
+        <ul className="feature-list mt-3">
+          <li>Create-Collaborate-Review-Secure-Publish-Maintain</li>
+          <li>Create a source of truth</li>
+          <li>Your content, your way</li>
+          <li>Create/Manage Environment</li>
+          <li>Create/Share internal API documentation</li>
+          <li>Scalability and Performance</li>
+        </ul>
+      </div>
 
-          <div className="login__main col-sm-6 col-md-7 col-lg-8 col-xl-9 p-0 mb-3 p-sm-4 p-xl-5">
-            <div className="login__main__loginbtn pt-sm-0">
-              <TECHDOCC className="d-flex justify-content-center d-sm-none w-100 mb-2" />
-              <h2 className="d-none d-sm-block t-dark">Welcome Back!</h2>
-              <div id={divId}></div>
-            </div>
-          </div>
+      <div className="login__main col-sm-6 col-md-7 col-lg-8 col-xl-9 p-0 mb-3 p-sm-4 p-xl-5">
+        <div className="login__main__loginbtn pt-sm-0">
+          <TECHDOCC className="d-flex justify-content-center d-sm-none w-100 mb-2" />
+          <h2 className="d-none d-sm-block t-dark">Welcome Back!</h2>
+          <div id={divId}></div>
         </div>
-      </body>
-    </html>
+      </div>
+    </div>
   );
 };
 
